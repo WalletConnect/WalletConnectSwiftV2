12cf73b18297af195063eac58bbd2db5f72e626d
llbartekll <bartus000@gmail.com>
2021-08-30 09:02:22 +0200

    Fix mocked transport

--- Tests/WalletConnect_SwiftTests/Mocks/MockedJSONRPCTransport.swift
+++ Tests/WalletConnect_SwiftTests/Mocks/MockedJSONRPCTransport.swift
@@ -4,6 +4,8 @@ import Foundation
 @testable import WalletConnect_Swift
 
 class MockedJSONRPCTransport: JSONRPCTransporting {
+    var onConnected: (() -> ())?
+    var onDisconnected: (() -> ())?
     var onPayload: ((String) -> ())?
     var send = false
     func send(_ string: String, completion: @escaping (Error?) -> ()) {
