3e860c95490ce940b9b3ca55edb7c65be1548752
André Vants <andrevants@icloud.com>
2021-09-01 11:17:36 -0300

    Renamed package on manifest

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "WalletConnect_Swift",
+    name: "WalletConnect",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15)
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "WalletConnect_Swift",
-            targets: ["WalletConnect_Swift"]),
+            name: "WalletConnect",
+            targets: ["WalletConnect"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,10 +24,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "WalletConnect_Swift",
+            name: "WalletConnect",
             dependencies: ["CryptoSwift"]),
         .testTarget(
-            name: "WalletConnect_SwiftTests",
-            dependencies: ["WalletConnect_Swift"]),
+            name: "WalletConnectTests",
+            dependencies: ["WalletConnect"]),
     ]
 )
