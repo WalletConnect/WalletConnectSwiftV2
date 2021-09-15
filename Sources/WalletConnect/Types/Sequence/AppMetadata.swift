ca46ec0a784d8412c14da4a15f7b8cf11e371362
llbartekll <>
2021-09-15 09:48:50 +0200

    add public initialisers to public structs

--- Sources/WalletConnect/Types/Sequence/AppMetadata.swift
+++ Sources/WalletConnect/Types/Sequence/AppMetadata.swift
@@ -1,7 +1,14 @@
 
 import Foundation
 
-struct AppMetadata: Codable, Equatable {
+public struct AppMetadata: Codable, Equatable {
+    public init(name: String?, description: String?, url: String?, icons: [String]?) {
+        self.name = name
+        self.description = description
+        self.url = url
+        self.icons = icons
+    }
+    
     let name: String?
     let description: String?
     let url: String?
