16450c95458e6832f39df911ea9d126e3fabe09d
André Vants <andrevants@icloud.com>
2021-09-20 11:55:29 -0300

    Added example app Xcode project

---
+++ Example/ExampleApp/SceneDelegate.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+}
