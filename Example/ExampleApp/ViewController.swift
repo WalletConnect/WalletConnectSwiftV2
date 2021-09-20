b05557254d3961afa977cbc65acafef3ce26b2e8
André Vants <andrevants@icloud.com>
2021-09-20 12:21:17 -0300

    Added WC dependency to example app

--- Example/ExampleApp/ViewController.swift
+++ Example/ExampleApp/ViewController.swift
@@ -1,7 +1,18 @@
 import UIKit
+import WalletConnect
 
 class ViewController: UIViewController {
 
+    let client: WalletConnectClient = {
+        let options = WalletClientOptions(
+            apiKey: "",
+            name: "Example",
+            isController: true,
+            metadata: AppMetadata(name: "Example App", description: nil, url: nil, icons: nil),
+            relayURL: URL(string: "wss://staging.walletconnect.org")!)
+        return WalletConnectClient(options: options)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
