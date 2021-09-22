f9e4bc57cd8a3151f16cfb1d440491f967e83e4f
André Vants <andrevants@icloud.com>
2021-09-22 13:34:47 -0300

    Add basic UI elements layout

--- Example/ExampleApp/Responder/SessionView.swift
+++ Example/ExampleApp/Responder/SessionView.swift
@@ -5,7 +5,7 @@ final class SessionView: UIView {
     let iconView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
-        imageView.backgroundColor = .systemRed
+        imageView.backgroundColor = .systemFill
         return imageView
     }()
     
@@ -15,12 +15,48 @@ final class SessionView: UIView {
         return label
     }()
     
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at eleifend est, vel porta enim. Praesent non placerat orci. Curabitur orci sem, molestie feugiat enim eu, tincidunt tincidunt est."
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let urlLabel: UILabel = {
+        let label = UILabel()
+        label.text = "decentralized.finance"
+        return label
+    }()
+    
+    let approveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Approve", for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 8
+        return button
+    }()
+    
+    let rejectButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Reject", for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .systemRed
+        button.layer.cornerRadius = 8
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .systemBackground
         
         addSubview(iconView)
         addSubview(nameLabel)
+        addSubview(descriptionLabel)
+        addSubview(urlLabel)
+        addSubview(approveButton)
+        addSubview(rejectButton)
         
         subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
         
@@ -32,6 +68,24 @@ final class SessionView: UIView {
             
             nameLabel.topAnchor.constraint(equalTo: iconView.bottomAnchor, constant: 32),
             nameLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 16),
+            descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            
+            urlLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
+            urlLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            approveButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            approveButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            approveButton.heightAnchor.constraint(equalToConstant: 44),
+            
+            rejectButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            rejectButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            rejectButton.heightAnchor.constraint(equalToConstant: 44),
+            
+            approveButton.widthAnchor.constraint(equalTo: rejectButton.widthAnchor),
+            rejectButton.leadingAnchor.constraint(equalTo: approveButton.trailingAnchor, constant: 16),
         ])
     }
     
