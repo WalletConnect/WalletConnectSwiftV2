d07fd2316ca5da3b70580e2cc58401d7c018bbba
André Vants <andrevants@icloud.com>
2021-09-28 09:45:40 -0300

    Add connect action to show a QR code

--- Example/ExampleApp/Proposer/ProposerViewController.swift
+++ Example/ExampleApp/Proposer/ProposerViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 final class ProposerViewController: UIViewController {
     
+    private var currentURI: String?
+    
     private let proposerView: ProposerView = {
         ProposerView()
     }()
@@ -13,8 +15,35 @@ final class ProposerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Dapp"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Connect",
+            style: .plain,
+            target: self,
+            action: #selector(connect)
+        )
+        
+        proposerView.copyButton.addTarget(self, action: #selector(copyURI), for: .touchUpInside)
+        proposerView.copyButton.isHidden = true
+    }
+    
+    @objc
+    private func connect() {
+        // TODO: Propose pairing and get generated URI
         let uriString = "wc:8097df5f14871126866252c1b7479a14aefb980188fc35ec97d130d24bd887c8@2?controller=false&publicKey=19c5ecc857963976fabb98ed6a3e0a6ab6b0d65c018b6e25fbdcd3a164def868&relay=%7B%22protocol%22%3A%22waku%22%7D"
-        proposerView.qrCodeView.image = generateQRCode(from: uriString)
+        currentURI = uriString
+        DispatchQueue.global().async { [weak self] in
+            if let qrImage = self?.generateQRCode(from: uriString) {
+                DispatchQueue.main.async {
+                    self?.proposerView.qrCodeView.image = qrImage
+                    self?.proposerView.copyButton.isHidden = false
+                }
+            }
+        }
+    }
+    
+    @objc func copyURI() {
+        // TODO: copy qr code uri to pasteboard
     }
     
     private func generateQRCode(from string: String) -> UIImage? {
@@ -26,34 +55,5 @@ final class ProposerViewController: UIViewController {
             }
         }
         return nil
-    }
-}
-
-final class ProposerView: UIView {
-    
-    let qrCodeView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .systemBackground
-        
-        addSubview(qrCodeView)
-        
-        subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
-        
-        NSLayoutConstraint.activate([
-            qrCodeView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 32),
-            qrCodeView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            qrCodeView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.6),
-            qrCodeView.widthAnchor.constraint(equalTo: qrCodeView.heightAnchor)
-        ])
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
