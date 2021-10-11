622e450d39b1034f74a02fbf9d123ef38d8592ed
llbartekll <>
2021-10-11 13:11:44 +0200

    try to fix build error

--- Sources/WalletConnect/Types/Session/SessionSequenceState.swift
+++ Sources/WalletConnect/Types/Session/SessionSequenceState.swift
@@ -17,5 +17,36 @@ extension SessionType {
                 return sequence.topic
             }
         }
+        static func == (lhs: SequenceState, rhs: SequenceState) -> Bool {
+            switch (lhs, rhs) {
+            case (.pending(let lhsPending), .pending(let rhsPending)):
+                return lhsPending.isEqual(to: rhsPending)
+            case (.settled(let lhsSettled), .settled(let rhsSettled)):
+                return lhsSettled.isEqual(to: rhsSettled)
+            default:
+                return false
+            }
+        }
+        
+        init(from decoder: Decoder) throws {
+            let values = try decoder.container(keyedBy: CodingKeys.self)
+            if let value = try? values.decode(SessionType.Settled.self, forKey: .settled) {
+                self = .settled(value)
+            } else if let value = try? values.decode(SessionType.Pending.self, forKey: .pending) {
+                self = .pending(value)
+            }
+            throw DecodingError.typeMismatch(Self.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Error"))
+        }
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            switch self {
+            case .settled(let value):
+                try container.encode(value, forKey: .settled)
+            case .pending(let value):
+                try container.encode(value, forKey: .pending)
+            }
+        }
     }
+    
 }
