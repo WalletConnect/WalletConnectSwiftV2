5adf9472851435448b7e17b4bd3a21853b9b8c89
André Vants <andrevants@icloud.com>
2021-10-22 18:31:05 -0300

    Refactor on URI object, add test cases

--- Sources/WalletConnect/Types/Pairing/PairParamsUri.swift
+++ Sources/WalletConnect/Types/Pairing/PairParamsUri.swift
@@ -1,6 +1,56 @@
 
 import Foundation
 
+struct WalletConnectURI: Equatable {
+    
+    let topic: String
+    let version: String
+    let publicKey: String
+    let isController: Bool
+    let relay: RelayProtocolOptions
+    
+    var absoluteString: String {
+        guard let relay = try? relay.json().addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
+            return ""
+        }
+        return "wc:\(topic)@\(version)?controller=\(isController)&publicKey=\(publicKey)&relay=\(relay)"
+    }
+    
+    init(topic: String, publicKey: String, isController: Bool, relay: RelayProtocolOptions) {
+        self.version = "2"
+        self.topic = topic
+        self.publicKey = publicKey
+        self.isController = isController
+        self.relay = relay
+    }
+    
+    init?(string: String) {
+        guard string.hasPrefix("wc:") else {
+            return nil
+        }
+        let urlString = !string.hasPrefix("wc://") ? string.replacingOccurrences(of: "wc:", with: "wc://") : string
+        guard let components = URLComponents(string: urlString) else {
+            return nil
+        }
+        
+        let query: [String: String]? = components.queryItems?.reduce(into: [:]) { $0[$1.name] = $1.value }
+        
+        guard let topic = components.user,
+              let version = components.host,
+              let publicKey = query?["publicKey"],
+              let isController = Bool(query?["controller"] ?? ""),
+              let relayOptions = query?["relay"],
+              let relay = try? JSONDecoder().decode(RelayProtocolOptions.self, from: Data(relayOptions.utf8))
+        else { return nil }
+        
+        self.version = version
+        self.topic = topic
+        self.publicKey = publicKey
+        self.isController = isController
+        self.relay = relay
+    }
+}
+
 extension PairingType {
     public struct UriParameters {
         let version: String
--- Tests/WalletConnectTests/PairingParamsUriTests.swift
+++ Tests/WalletConnectTests/PairingParamsUriTests.swift
@@ -3,10 +3,39 @@ import XCTest
 
 final class PairingParamsUriTests: XCTestCase {
     func testInitFromUriAndConvertToUri() {
-        let uriParameters = PairingType.UriParameters(uri)!
+        let uriParameters = PairingType.UriParameters(stubURI)!
         let uriConverted = uriParameters.absoluteString()!
-        XCTAssertEqual(uri, uriConverted)
+        XCTAssertEqual(stubURI, uriConverted)
     }
 }
 
-fileprivate let uri = "wc:8097df5f14871126866252c1b7479a14aefb980188fc35ec97d130d24bd887c8@2?controller=false&publicKey=19c5ecc857963976fabb98ed6a3e0a6ab6b0d65c018b6e25fbdcd3a164def868&relay=%7B%22protocol%22%3A%22waku%22%7D"
+fileprivate let stubURI = "wc:8097df5f14871126866252c1b7479a14aefb980188fc35ec97d130d24bd887c8@2?controller=false&publicKey=19c5ecc857963976fabb98ed6a3e0a6ab6b0d65c018b6e25fbdcd3a164def868&relay=%7B%22protocol%22%3A%22waku%22%7D"
+
+final class WalletConnectURITests: XCTestCase {
+    
+    func testInitURIToString() {
+        let inputURI = WalletConnectURI(
+            topic: "8097df5f14871126866252c1b7479a14aefb980188fc35ec97d130d24bd887c8",
+            publicKey: "19c5ecc857963976fabb98ed6a3e0a6ab6b0d65c018b6e25fbdcd3a164def868",
+            isController: true,
+            relay: RelayProtocolOptions(protocol: "waku", params: nil))
+        let uriString = inputURI.absoluteString
+        let outputURI = WalletConnectURI(string: uriString)
+        XCTAssertEqual(inputURI, outputURI)
+    }
+    
+    func testInitStringToURI() {
+        let inputURIString = stubURI
+        let uri = WalletConnectURI(string: inputURIString)
+        let outputURIString = uri?.absoluteString
+        XCTAssertEqual(inputURIString, outputURIString)
+    }
+    
+    func testInitStringToURIAlternate() {
+        let expectedString = stubURI
+        let inputURIString = expectedString.replacingOccurrences(of: "wc:", with: "wc://")
+        let uri = WalletConnectURI(string: inputURIString)
+        let outputURIString = uri?.absoluteString
+        XCTAssertEqual(expectedString, outputURIString)
+    }
+}
