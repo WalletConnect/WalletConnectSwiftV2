d1c7893315a14e55e09e3818475d6e81ed026b13
André Vants <andrevants@icloud.com>
2021-11-08 17:16:38 -0300

    Testing expiration case

--- Sources/WalletConnect/Extensions/String.swift
+++ Sources/WalletConnect/Extensions/String.swift
@@ -3,6 +3,21 @@
 import Foundation
 
 extension String {
+    static func generateTopic() -> String? {
+        var keyData = Data(count: 32)
+        let result = keyData.withUnsafeMutableBytes {
+            SecRandomCopyBytes(kSecRandomDefault, 32, $0.baseAddress!)
+        }
+        if result == errSecSuccess {
+            return keyData.toHexString()
+        } else {
+            print("Problem generating random bytes")
+            return nil
+        }
+    }
+}
+
+extension String {
     func toHexEncodedString(uppercase: Bool = true, prefix: String = "", separator: String = "") -> String {
         return unicodeScalars.map { prefix + .init($0.value, radix: 16, uppercase: uppercase) } .joined(separator: separator)
     }
--- Tests/WalletConnectTests/SequenceStoreTests.swift
+++ Tests/WalletConnectTests/SequenceStoreTests.swift
@@ -1,18 +1,74 @@
 import XCTest
 @testable import WalletConnect
 
-struct SequenceStub: WCSequence {
+struct WCSequenceStub: WCSequence, Equatable {
     let topic: String
     let expiryDate: Date
 }
 
+final class TimeTraveler {
+    
+    private(set) var referenceDate = Date()
+    
+    func generateDate() -> Date {
+        return referenceDate
+    }
+    
+    func travel(by timeInterval: TimeInterval) {
+        referenceDate = referenceDate.addingTimeInterval(timeInterval)
+    }
+}
+
 final class SequenceStoreTests: XCTestCase {
     
-    var sut: SequenceStore<SequenceStub>!
+    var sut: SequenceStore<WCSequenceStub>!
     
     var storageFake: RuntimeKeyValueStorage!
     
+    var timeTraveler: TimeTraveler!
+    
+    let defaultTime = TimeInterval(Time.day)
+    
     override func setUp() {
+        timeTraveler = TimeTraveler()
+        storageFake = RuntimeKeyValueStorage()
+        sut = SequenceStore<WCSequenceStub>(storage: storageFake, dateInitializer: timeTraveler.generateDate)
+    }
+    
+    override func tearDown() {
+        timeTraveler = nil
+        storageFake = nil
+        sut = nil
+    }
+    
+    private func stubSequence() -> WCSequenceStub {
+        WCSequenceStub(
+            topic: String.generateTopic()!,
+            expiryDate: timeTraveler.referenceDate.addingTimeInterval(defaultTime)
+        )
+    }
+    
+    func testRoundTrip() {
+        let sequence = stubSequence()
+        try? sut.setSequence(sequence)
+        let retrieved = try? sut.getSequence(forTopic: sequence.topic)
+        XCTAssertEqual(retrieved, sequence)
+    }
+    
+    func testExpiration() {
+        let sequence = stubSequence()
+        var expiredTopic: String? = nil
+        sut.onSequenceExpiration = { expiredTopic = $0 }
+        
+        try? sut.setSequence(sequence)
+        timeTraveler.travel(by: defaultTime)
+        let retrieved = try? sut.getSequence(forTopic: sequence.topic)
+        
+        XCTAssertNil(retrieved)
+        XCTAssertEqual(expiredTopic, sequence.topic)
+    }
+    
+    func testDelete() {
         
     }
 }
