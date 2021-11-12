5d76f5c26cf4c3ecb83e1daa17eb44453b81ce26
Bartosz Rozwarski <bartus000@gmail.com>
2021-11-12 13:30:26 +0100

    savepoint

---
+++ Sources/WalletConnect/Relay/JsonRpcHistory.swift
@@ -0,0 +1,18 @@
+
+
+import Foundation
+
+struct JsonRpcRecord: Codable {
+    
+}
+
+class JsonRpcHistory {
+    var recordsStorage:
+    func get() -> String {
+        
+    }
+    
+    func set() -> String {
+        
+    }
+}
