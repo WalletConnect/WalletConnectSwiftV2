2e28a1c0057c40d0c06fd480e7c1249e6e7b8e1f
Bartosz Rozwarski <bartus000@gmail.com>
2021-12-09 14:06:30 +0100

    remove unused code

--- Tests/WalletConnectTests/Helpers/Error+Extension.swift
+++ Tests/WalletConnectTests/Helpers/Error+Extension.swift
@@ -7,30 +7,3 @@ extension NSError {
         NSError(domain: "com.walletconnect.sdk.tests.error", code: code, userInfo: nil)
     }
 }
-
-//extension Error {
-//    
-//    var asNetworkError: NetworkError? {
-//        return self as? NetworkError
-//    }
-//}
-//
-//extension NetworkError {
-//
-//    var isWebSocketError: Bool {
-//        guard case .webSocketNotConnected = self else { return false }
-//        return true
-//    }
-//    
-//    var isSendMessageError: Bool {
-//        guard case .sendMessageFailed = self else { return false }
-//        return true
-//    }
-//    
-//    var isReceiveMessageError: Bool {
-//        guard case .receiveMessageFailure = self else { return false }
-//        return true
-//    }
-//}
-//
-//extension String: Error {}
