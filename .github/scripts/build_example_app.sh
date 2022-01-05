d15ae493727e8bed23830e294355fd0e66e72ff6
Bartosz Rozwarski <bartus000@gmail.com>
2022-01-05 12:56:41 +0100

    final clean up

--- .github/scripts/build_example_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bundle exec fastlane gym 
-set -eo pipefail
-xcodebuild  -project Example/ExampleApp.xcodeproj \
--scheme ExampleApp
--allowProvisioningUpdates \
--destination platform=iOS\ Simulator,OS=15.1 \
-clean
