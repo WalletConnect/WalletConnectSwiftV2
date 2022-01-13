acc8f6561631472a7998b09b541cf2e06ccc3ce7
Bartosz Rozwarski <bartus000@gmail.com>
2022-01-13 10:53:14 +0100

    add signing

--- Example/ExampleApp/Responder/ResponderViewController.swift
+++ Example/ExampleApp/Responder/ResponderViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 import WalletConnect
 import WalletConnectUtils
+import Web3
+import CryptoSwift
 
 final class ResponderViewController: UIViewController {
 
@@ -18,9 +20,10 @@ final class ResponderViewController: UIViewController {
             clientName: "responder"
         )
     }()
-    let account = "0x022c0c42a80bd19EA4cF0F94c4F9F96645759716"
+    lazy  var account = privateKey.address.hex(eip55: true)
     var sessionItems: [ActiveSessionItem] = []
     var currentProposal: Session.Proposal?
+    let privateKey: EthereumPrivateKey = try! EthereumPrivateKey(hexPrivateKey: "0xe56da0e170b5e09a8bb8f1b693392c7d56c3739a9c75740fbc558a2877868540")
     
     private let responderView: ResponderView = {
         ResponderView()
@@ -78,10 +81,10 @@ final class ResponderViewController: UIViewController {
     
     private func showSessionRequest(_ sessionRequest: Request) {
         let requestVC = RequestViewController(sessionRequest)
-        requestVC.onSign = { [weak self] in
-            let result = "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
-            let response = JSONRPCResponse<AnyCodable>(id: sessionRequest.id, result: AnyCodable(result))
-            self?.client.respond(topic: sessionRequest.topic, response: .response(response))
+        requestVC.onSign = { [unowned self] in
+            let result = signEth(request: sessionRequest)
+            let response = JSONRPCResponse<AnyCodable>(id: sessionRequest.id, result: result)
+            client.respond(topic: sessionRequest.topic, response: .response(response))
         }
         requestVC.onReject = { [weak self] in
             self?.client.respond(topic: sessionRequest.topic, response: .error(JSONRPCErrorResponse(id: sessionRequest.id, error: JSONRPCErrorResponse.Error(code: 0, message: ""))))
@@ -224,24 +227,71 @@ extension ResponderViewController: WalletConnectClientDelegate {
             self.responderView.tableView.reloadData()
         }
     }
-}
-
-extension UIAlertController {
     
-    static func createInputAlert(confirmHandler: @escaping (String) -> Void) -> UIAlertController {
-        let alert = UIAlertController(title: "Paste URI", message: "Enter a WalletConnect URI to connect.", preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        let confirmAction = UIAlertAction(title: "Connect", style: .default) { _ in
-            if let input = alert.textFields?.first?.text, !input.isEmpty {
-                confirmHandler(input)
-            }
+
+    
+    func signEth(request: Request) -> AnyCodable {
+        let method = request.method
+        if method == "eth_sendTransaction" {
+        } else if method == "personal_sign" {
+            let params = try! request.params.get([String].self)
+            let messageToSign = params[0]
+            let signHash = signHash(messageToSign)
+            let (v, r, s) = try! self.privateKey.sign(hash: signHash)
+            let result = "0x" + r.toHexString() + s.toHexString() + String(v + 27, radix: 16)
+            return AnyCodable(result)
+        } else if method == "eth_signTypedData" {
+            let params = try! request.params.get([String].self)
+            print(params)
+            let messageToSign = params[1]
+            let signHash = signHash(messageToSign)
+            let (v, r, s) = try! self.privateKey.sign(hash: signHash)
+            let result = r.toHexString() + s.toHexString() + String(v + 27, radix: 16)
+            return AnyCodable(result)
+            
+        } else if method == "eth_sendTransaction" {
+
+        } else {
+            print("TODO")
         }
-        alert.addTextField { textField in
-            textField.placeholder = "wc://a14aefb980188fc35ec9..."
-        }
-        alert.addAction(cancelAction)
-        alert.addAction(confirmAction)
-        alert.preferredAction = confirmAction
-        return alert
+        fatalError()
+    }
+    
+    func signHash(_ message: String) -> Bytes {
+        let prefix = "\u{19}Ethereum Signed Message:\n"
+        let messageData = Data(hex: message)
+        let prefixData = (prefix + String(messageData.count)).data(using: .utf8)!
+        let prefixedMessageData = prefixData + messageData
+        let dataToHash: Bytes = .init(hex: prefixedMessageData.toHexString())
+        return SHA3(variant: .keccak256).calculate(for: dataToHash)
     }
 }
+
+//struct EthSignTypedData: Codable {
+//    case address(String)
+//    case message(Payload)
+//}
+//
+//extension EthSignTypedData {
+//    struct Payload: Codable {
+//        let types: Types
+//        let primaryType: String
+//        let domain: Domain
+//        let message: Message
+//    }
+//
+//    struct Types: Codable {
+//        let EIP712Domain: [EIP712Domain]
+//        let Person: Person
+//        let Mail: Mail
+//    }
+//
+//    struct EIP712Domain: Codable {
+//        let name: String
+//        let type: String
+//    }
+//
+//    struct Domain: Codable {
+//
+//    }
+//}
