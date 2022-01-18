54adf5d00af1ae0486769223ab88b40af6425082
André Vants <andrevants@icloud.com>
2022-01-18 12:34:03 -0300

    CAIP-10 account ID validation for strings

--- Sources/WalletConnect/Extensions/String.swift
+++ Sources/WalletConnect/Extensions/String.swift
@@ -4,16 +4,30 @@ import Foundation
 
 extension String {
     
+    static let chainNamespaceRegex = "^[-a-z0-9]{3,8}$"
+    static let chainReferenceRegex = "^[-a-zA-Z0-9]{1,32}$"
+    static let accountAddressRegex = "^[a-zA-Z0-9]{1,64}$"
+    
     static func conformsToCAIP2(_ string: String) -> Bool {
-        let namespaceRegex = "^[-a-z0-9]{3,8}$"
-        let referenceRegex = "^[-a-zA-Z0-9]{1,32}$"
         let splits = string.split(separator: ":", omittingEmptySubsequences: false)
         guard splits.count == 2 else { return false }
         let namespace = splits[0]
         let reference = splits[1]
-        let isNamespaceValid = (namespace.range(of: namespaceRegex, options: .regularExpression) != nil)
-        let isReferenceValid = (reference.range(of: referenceRegex, options: .regularExpression) != nil)
+        let isNamespaceValid = (namespace.range(of: chainNamespaceRegex, options: .regularExpression) != nil)
+        let isReferenceValid = (reference.range(of: chainReferenceRegex, options: .regularExpression) != nil)
         return isNamespaceValid && isReferenceValid
+    }
+    
+    static func conformsToCAIP10(_ string: String) -> Bool {
+        let splits = string.split(separator: ":", omittingEmptySubsequences: false)
+        guard splits.count == 3 else { return false }
+        let namespace = splits[0]
+        let reference = splits[1]
+        let address = splits[2]
+        let isNamespaceValid = (namespace.range(of: chainNamespaceRegex, options: .regularExpression) != nil)
+        let isReferenceValid = (reference.range(of: chainReferenceRegex, options: .regularExpression) != nil)
+        let isAddressValid = (address.range(of: accountAddressRegex, options: .regularExpression) != nil)
+        return isNamespaceValid && isReferenceValid && isAddressValid
     }
     
     static func generateTopic() -> String? {
