b451acae2d86c54c018f0853079bd011e2cb919e
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-01-20 07:41:53 +0900

    Fix indentation

--- Sources/WalletConnect/Codec/AES_CBC/CBCError.swift
+++ Sources/WalletConnect/Codec/AES_CBC/CBCError.swift
@@ -9,27 +9,27 @@ import Foundation
 import CryptoKit
 
 internal struct CBCError: LocalizedError {
-	let message: String
-	let status: Int32
-	
-	var errorDescription: String? {
-		return "CBC Error: \"\(message)\", status: \(status)"
-	}
+    let message: String
+    let status: Int32
+
+    var errorDescription: String? {
+        return "CBC Error: \"\(message)\", status: \(status)"
+    }
 }
 
 
 public extension Data {
-	var bytes: [UInt8] {
-		[UInt8](self)
-	}
+    var bytes: [UInt8] {
+        [UInt8](self)
+    }
 }
 
 public extension SymmetricKey {
-	/// A Data instance created safely from the contiguous bytes without making any copies.
-	var dataRepresentation: Data {
-		return withUnsafeBytes { bytes in
-			let cfdata = CFDataCreateWithBytesNoCopy(nil, bytes.baseAddress?.assumingMemoryBound(to: UInt8.self), bytes.count, kCFAllocatorNull)
-			return (cfdata as Data?) ?? Data()
-		}
-	}
+    /// A Data instance created safely from the contiguous bytes without making any copies.
+    var dataRepresentation: Data {
+        return withUnsafeBytes { bytes in
+            let cfdata = CFDataCreateWithBytesNoCopy(nil, bytes.baseAddress?.assumingMemoryBound(to: UInt8.self), bytes.count, kCFAllocatorNull)
+            return (cfdata as Data?) ?? Data()
+        }
+    }
 }
