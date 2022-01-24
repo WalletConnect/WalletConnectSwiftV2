3f4c06ca32398a51c540a6bad5729682db81a6fb
André Vants <andrevants@icloud.com>
2022-01-24 10:01:30 -0300

    Add enum for reason codes

--- Sources/WalletConnect/Reason.swift
+++ Sources/WalletConnect/Reason.swift
@@ -9,3 +9,33 @@ public struct Reason {
         self.message = message
     }
 }
+
+
+
+enum ReasonCode {
+    case generic(message: String)
+    case invalidUpdateRequest(String)
+    
+    var code: Int {
+        switch self {
+        case .generic: return 0
+        case .invalidUpdateRequest: return 1003
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .generic(let message): return message
+        case .invalidUpdateRequest(let context): return "Invalid \(context) update request"
+        }
+    }
+}
+
+//code: 1301
+//message: "No matching ${context} with topic: ${topic}"
+
+//code: 3003
+//message: "Unauthorized ${context} update request"
+
+//code: 3005
+//message: "Unauthorized: peer is also ${"" | "not"} controller"
