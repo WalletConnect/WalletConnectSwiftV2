877482ad7016a159133d2685031599d9a90259e5
André Vants <andrevants@icloud.com>
2022-02-02 17:20:37 -0300

    Account type documentation

--- Sources/WalletConnect/Account.swift
+++ Sources/WalletConnect/Account.swift
@@ -1,27 +1,47 @@
 /**
+ A value that identifies an account in any given blockchain.
  
+ This structure parses account IDs according to [CAIP-10].
+ Account IDs are prefixed with a [CAIP-2] blockchain ID, delimited by a `':'` character, followed by the account address.
+ 
+ Specifying a blockchain account by using a chain-agnostic identifier is useful to allow interoperability between multiple
+ chains when using both wallets and decentralized applications.
+ 
+ [CAIP-2]:https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-2.md
+ [CAIP-10]:https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-10.md
  */
 public struct Account {
     
-    ///
-    public let namespace: String
+    /// A blockchain namespace. Usually describes an ecosystem or standard.
+    public var namespace: String
     
-    ///
-    public let reference: String
+    /// A reference string that identifies a blockchain within a given namespace.
+    public var reference: String
     
-    ///
-    public let address: String
+    /// The account's address specific to the blockchain.
+    public var address: String
     
-    ///
+    /// The CAIP-2 blockchain identifier of the account.
     public var blockchainIdentifier: String {
         "\(namespace):\(reference)"
     }
     
-    ///
+    /// The CAIP-10 account identifier absolute string.
     public var absoluteString: String {
         "\(namespace):\(reference):\(address)"
     }
     
+    /// Returns whether the account conforms to CAIP-10.
+    public var isCAIP10Conformant: Bool {
+        String.conformsToCAIP10(absoluteString)
+    }
+    
+    /**
+     Creates an account instance from the provided string.
+     
+     This initializer returns nil if the string doesn't represent a valid account id in conformance with
+     [CAIP-10](https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-10.md).
+     */
     public init?(string: String) {
         guard String.conformsToCAIP10(string) else { return nil }
         let splits = string.split(separator: ":")
