a6d0ed4da3be4f53ace53a208bb970df37ed7168
Bartosz Rozwarski <bartus000@gmail.com>
2022-02-04 13:46:52 +0100

    invalidate task

--- Sources/Relayer/SocketConnectionHandler/BackgroundTaskRegistering.swift
+++ Sources/Relayer/SocketConnectionHandler/BackgroundTaskRegistering.swift
@@ -13,6 +13,7 @@ class BackgroundTaskRegistrar: BackgroundTaskRegistering {
 
     func register(name: String, completion: @escaping () -> ()) {
 #if os(iOS)
+        backgroundTaskID = .invalid
         backgroundTaskID = UIApplication.shared.beginBackgroundTask (withName: name) { [unowned self] in
             UIApplication.shared.endBackgroundTask(backgroundTaskID)
             backgroundTaskID = .invalid
