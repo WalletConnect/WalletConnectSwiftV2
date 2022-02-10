dc241adbf0e7c645b00836de25c47f7fa180795f
André Vants <andrevants@icloud.com>
2022-02-10 15:00:19 -0300

    New alternate initializers for Account type

--- Tests/WalletConnectTests/AccountTests.swift
+++ Tests/WalletConnectTests/AccountTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class AccountTests: XCTestCase {
     
-    func testInit() {
+    func testInitFromString() {
         // Valid accounts
         XCTAssertNotNil(Account("std:0:0"))
         XCTAssertNotNil(Account("chainstd:8c3444cf8970a9e41a706fab93e7a6c4:6d9b0b4b9994e8a6afbd3dc3ed983cd51c755afb27cd1dc7825ef59c134a39f7"))
@@ -12,6 +12,24 @@ final class AccountTests: XCTestCase {
         XCTAssertNil(Account("std:0:$"))
         XCTAssertNil(Account("std:$:0"))
         XCTAssertNil(Account("st:0:0"))
+    }
+    
+    func testInitFromChainAndAddress() {
+        // Valid accounts
+        XCTAssertNotNil(Account(chainIdentifier: "std:0", address: "0"))
+        XCTAssertNotNil(Account(chainIdentifier: "chainstd:8c3444cf8970a9e41a706fab93e7a6c4", address: "6d9b0b4b9994e8a6afbd3dc3ed983cd51c755afb27cd1dc7825ef59c134a39f7"))
+        
+        // Invalid accounts
+        XCTAssertNil(Account(chainIdentifier: "std:0", address: ""))
+        XCTAssertNil(Account(chainIdentifier: "std", address: "0"))
+    }
+    
+    func testInitCAIP10Conformance() {
+        XCTAssertTrue(Account(namespace: "std", reference: "0", address: "0").isCAIP10Conformant)
+        
+        XCTAssertFalse(Account(namespace: "st", reference: "0", address: "0").isCAIP10Conformant)
+        XCTAssertFalse(Account(namespace: "std", reference: "", address: "0").isCAIP10Conformant)
+        XCTAssertFalse(Account(namespace: "std", reference: "0", address: "").isCAIP10Conformant)
     }
     
     func testBlockchainIdentifier() {
