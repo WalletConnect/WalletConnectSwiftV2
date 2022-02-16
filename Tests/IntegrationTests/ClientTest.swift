87a7c6a3283bc61e738dfb5cd88221e4680b6e1c
Bartosz Rozwarski <bartus000@gmail.com>
2022-02-16 12:40:18 +0100

    fix pairing update integration test

--- Tests/IntegrationTests/ClientTest.swift
+++ Tests/IntegrationTests/ClientTest.swift
@@ -341,8 +341,8 @@ final class ClientTests: XCTestCase {
         let permissions = Session.Permissions.stub()
         let uri = try! proposer.client.connect(sessionPermissions: permissions)!
         _ = try! responder.client.pair(uri: uri)
-        proposer.onPairingUpdate = { _, appMetadata in
-            XCTAssertNotNil(appMetadata)
+        proposer.onPairingUpdate = { pairing in
+            XCTAssertNotNil(pairing.peer)
             proposerReceivesPairingUpdateExpectation.fulfill()
         }
         waitForExpectations(timeout: defaultTimeout, handler: nil)
