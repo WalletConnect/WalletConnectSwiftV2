4db02e01515f8a65325f7f3c4d1f938b8c6cc753
Bartosz Rozwarski <bartus000@gmail.com>
2022-02-23 15:39:09 +0100

    fix failing WalletConnectURI tests

--- Tests/WalletConnectTests/WalletConnectURITests.swift
+++ Tests/WalletConnectTests/WalletConnectURITests.swift
@@ -2,10 +2,10 @@ import XCTest
 @testable import WalletConnect
 
 fileprivate let stubTopic = "8097df5f14871126866252c1b7479a14aefb980188fc35ec97d130d24bd887c8"
-fileprivate let stubPubKey = "19c5ecc857963976fabb98ed6a3e0a6ab6b0d65c018b6e25fbdcd3a164def868"
-fileprivate let stubProtocol = "%7B%22protocol%22%3A%22waku%22%7D"
+fileprivate let stubSymKey = "587d5484ce2a2a6ee3ba1962fdd7e8588e06200c46823bd18fbd67def96ad303"
+fileprivate let stubProtocol = "waku"
 
-fileprivate let stubURI = "wc:\(stubTopic)@2?controller=false&publicKey=\(stubPubKey)&relay=\(stubProtocol)"
+fileprivate let stubURI = "wc:7f6e504bfad60b485450578e05678ed3e8e8c4751d3c6160be17160d63ec90f9@2?symKey=587d5484ce2a2a6ee3ba1962fdd7e8588e06200c46823bd18fbd67def96ad303&relay-protocol=waku"
 
 final class WalletConnectURITests: XCTestCase {
     
@@ -47,13 +47,13 @@ final class WalletConnectURITests: XCTestCase {
     }
     
     func testInitFailsNoSymKeyParam() {
-        let inputURIString = stubURI.replacingOccurrences(of: "&symKey=\(stubPubKey)", with: "")
+        let inputURIString = stubURI.replacingOccurrences(of: "symKey=\(stubSymKey)", with: "")
         let uri = WalletConnectURI(string: inputURIString)
         XCTAssertNil(uri)
     }
     
     func testInitFailsNoRelayParam() {
-        let inputURIString = stubURI.replacingOccurrences(of: "&relay=\(stubProtocol)", with: "")
+        let inputURIString = stubURI.replacingOccurrences(of: "&relay-protocol=\(stubProtocol)", with: "")
         let uri = WalletConnectURI(string: inputURIString)
         XCTAssertNil(uri)
     }
