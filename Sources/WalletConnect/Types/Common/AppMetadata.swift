d3111045c68699fc3fbc4a2cf8891e1bf110c637
André Vants <andrevants@icloud.com>
2022-03-03 13:47:31 -0300

    Adds code documentation to AppMetadata

--- Sources/WalletConnect/Types/Common/AppMetadata.swift
+++ Sources/WalletConnect/Types/Common/AppMetadata.swift
@@ -1,17 +1,42 @@
 
 import Foundation
 
-/// App metadata object that describes application metadata.
+/**
+ A structure that identifies a peer connected through a WalletConnect session.
+ 
+ You can provide human-readable information about your app so that it can be shared with a connected peer, for example,
+ during a session proposal event.
+ 
+ This information should make the identity of your app clear to the end-user and easily verifiable. Therefore, it is a
+ suitable place to briefly communicate your brand.
+ */
 public struct AppMetadata: Codable, Equatable {
+    
+    /// The name of the app.
+    public let name: String?
+    
+    /// A brief textual description of the app that can be displayed to peers.
+    public let description: String?
+    
+    /// The URL string that identifies the official domain of the app.
+    public let url: String?
+    
+    /// An array of URL strings pointing to the icon assets on the web.
+    public let icons: [String]?
+    
+    /**
+     Creates a new metadata object with the specified information.
+     
+     - parameters:
+        - name: The name of the app.
+        - description: A brief textual description of the app that can be displayed to peers.
+        - url: The URL string that identifies the official domain of the app.
+        - icons: An array of URL strings pointing to the icon assets on the web.
+     */
     public init(name: String?, description: String?, url: String?, icons: [String]?) {
         self.name = name
         self.description = description
         self.url = url
         self.icons = icons
     }
-    
-    public let name: String?
-    public let description: String?
-    public let url: String?
-    public let icons: [String]?
 }
