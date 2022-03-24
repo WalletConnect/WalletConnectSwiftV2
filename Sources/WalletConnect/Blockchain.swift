ef68055063e2c2376f9af2b910f12e350babdb2a
André Vants <andrevants@icloud.com>
2022-03-24 14:19:19 +0100

    Adds code documentation to the blockchain type

--- Sources/WalletConnect/Blockchain.swift
+++ Sources/WalletConnect/Blockchain.swift
@@ -1,13 +1,32 @@
+/**
+ A value that identifies a blockchain.
+ 
+ This structure parses chain IDs according to [CAIP-2]. A chain ID references a specific blockchain in the ecosystem.
+ 
+ By using chain-agnostic identifiers, different blockchains can be referenced in a standardized way, which aims to provide
+ common semantics to locate resources in the decentralized space in a human readable and transaction-friendly way.
+ 
+ [CAIP-2]:https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-2.md
+ */
 public struct Blockchain: Equatable {
     
+    /// A blockchain namespace. Usually describes an ecosystem or standard.
     public let namespace: String
     
+    /// A reference string that identifies a blockchain within a given namespace.
     public let reference: String
     
+    /// The CAIP-2 blockchain identifier string.
     public var absoluteString: String {
         "\(namespace):\(reference)"
     }
     
+    /**
+     Creates an instance of a blockchain reference from a string.
+     
+     This initializer returns nil if the string doesn't represent a valid chain id in conformance with
+     [CAIP-2](https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-2.md)
+     */
     public init?(_ string: String) {
         guard String.conformsToCAIP2(string) else { return nil }
         let splits = string.split(separator: ":")
@@ -15,6 +34,12 @@ public struct Blockchain: Equatable {
         self.reference = String(splits[1])
     }
     
+    /**
+     Creates an instance of a blockchain reference from a namespace and a reference string.
+     
+     This initializer returns nil if the `namespace` or `reference` strings formats are invalid, according to
+     [CAIP-2](https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-2.md)
+     */
     public init?(namespace: String, reference: String) {
         self.init("\(namespace):\(reference)")
     }
