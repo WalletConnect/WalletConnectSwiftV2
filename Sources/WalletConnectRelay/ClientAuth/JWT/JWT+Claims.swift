0e5f0186d0b63fa32b396d26839ee95ba459dea1
Bartosz Rozwarski <bartus000@gmail.com>
2022-07-05 10:32:11 +0200

    fix bug in encoder

--- Sources/WalletConnectRelay/ClientAuth/JWT/JWT+Claims.swift
+++ Sources/WalletConnectRelay/ClientAuth/JWT/JWT+Claims.swift
@@ -10,6 +10,7 @@ extension JWT {
 
         func encode() throws -> String {
             let jsonEncoder = JSONEncoder()
+            jsonEncoder.outputFormatting = .withoutEscapingSlashes
             jsonEncoder.dateEncodingStrategy = .secondsSince1970
             let data = try jsonEncoder.encode(self)
             return JWTEncoder.base64urlEncodedString(data: data)
