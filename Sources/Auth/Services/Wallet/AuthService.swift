aecff99728f5ac810fb46ca0fd6550954afbfbe8
Artur Guseinov <arthur.guseinov@gmail.com>
2022-07-29 13:24:00 +0300

    AuthService -> AuthClient

--- Sources/Auth/Services/Wallet/AuthService.swift
+++ Sources/Auth/Services/Wallet/AuthService.swift
@@ -1,30 +1,6 @@
 import Foundation
 
 actor AuthService {
-    enum Errors: Error {
-        case malformedPairingURI
-    }
-
-    private let appPairService: AppPairService
-
-    private let walletPairService: WalletPairService
-
-    init(appPairService: AppPairService, walletPairService: WalletPairService) {
-        self.appPairService = appPairService
-        self.walletPairService = walletPairService
-    }
-
-    func connect() async throws -> String {
-        let uri = try await appPairService.create()
-        return uri.absoluteString
-    }
-
-    func pair(uri: String) async throws {
-        guard let pairingURI = WalletConnectURI(string: uri) else {
-            throw Errors.malformedPairingURI
-        }
-        try await walletPairService.pair(pairingURI)
-    }
 
     func respond(respondParams: RespondParams) async throws {
 
