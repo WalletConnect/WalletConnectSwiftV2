c2a25077a03cd79b6f069714227098f3eed256a3
Bartosz Rozwarski <bartus000@gmail.com>
2022-08-09 11:05:51 +0200

    run lint

--- Tests/AuthTests/SIWEMessageFormatterTests.swift
+++ Tests/AuthTests/SIWEMessageFormatterTests.swift
@@ -6,7 +6,6 @@ class SIWEMessageFormatterTests: XCTestCase {
     var sut: SIWEMessageFormatter!
     let address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 
-
     override func setUp() {
         sut = SIWEMessageFormatter()
     }
