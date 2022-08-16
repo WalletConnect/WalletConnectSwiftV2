356c62fdce4639e4815ee14417d045318b43a2a4
Maisa <maisa.milena@gmail.com>
2022-08-15 22:28:34 -0300

    Update with RC comments

--- Tests/WalletConnectSignTests/WCPairingTests.swift
+++ Tests/WalletConnectSignTests/WCPairingTests.swift
@@ -75,21 +75,27 @@ final class WCPairingTests: XCTestCase {
     }
     
     func testUpdateExpiry_WhenNewExpiryDateIsLessThanExpiryDate_ShouldThrowInvalidUpdateExpiryValue() {
-        let expiryDate = referenceDate.advanced(by: 10 * .minute)
+        let expiryDate = referenceDate.advanced(by: 40 * .day)
         var pairing = WCPairing(topic: "", relay: .stub(), peerMetadata: .stub(), expiryDate: expiryDate)
-        XCTAssertThrowsError(try pairing.updateExpiry(40 * .day)) { error in
+        XCTAssertThrowsError(try pairing.updateExpiry(10 * .minute)) { error in
             XCTAssertEqual(error as! WCPairing.Errors, WCPairing.Errors.invalidUpdateExpiryValue)
         }
     }
 
-    func testActivate_WhenUpdateExpiryFails_ShouldActivateAndThrowError() {
-        let expiryDate = referenceDate.advanced(by: 10 * .minute)
+    func testActivate_WhenCanUpdateExpiry_ShouldActivateAndUpdateExpiryIn30Days() {
+        var pairing = WCPairing(topic: "", relay: .stub(), peerMetadata: .stub(), expiryDate: referenceDate)
+        XCTAssertFalse(pairing.active)
+        pairing.activate()
+        XCTAssertTrue(pairing.active)
+        XCTAssertEqual(referenceDate.advanced(by: 30 * .day), pairing.expiryDate)
+    }
+    
+    func testActivate_WhenUpdateExpiryIsInvalid_ShouldActivateAndNotUpdateExpiry() {
+        let expiryDate = referenceDate.advanced(by: 40 * .day)
         var pairing = WCPairing(topic: "", relay: .stub(), peerMetadata: .stub(), expiryDate: expiryDate)
         XCTAssertFalse(pairing.active)
         pairing.activate()
-        XCTAssertThrowsError(try pairing.updateExpiry(10 * .minute)) { error in
-            XCTAssertEqual(error as! WCPairing.Errors, WCPairing.Errors.invalidUpdateExpiryValue)
-        }
         XCTAssertTrue(pairing.active)
+        XCTAssertEqual(expiryDate, pairing.expiryDate)
     }
 }
