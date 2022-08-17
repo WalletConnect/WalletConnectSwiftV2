57b5400b411e71922167fa6b9c7a1e4f0eb82bfc
Bartosz Rozwarski <bartus000@gmail.com>
2022-08-17 11:10:07 +0200

    fix build

--- Sources/Auth/AuthClientFactory.swift
+++ Sources/Auth/AuthClientFactory.swift
@@ -19,7 +19,7 @@ public struct AuthClientFactory {
         let kms = KeyManagementService(keychain: keychainStorage)
         let serializer = Serializer(kms: kms)
         let history = RPCHistory(keyValueStore: historyStorage)
-        let networkingInteractor = NetworkingInteractor(relayClient: relayClient, serializer: serializer, rpcHistory: history)
+        let networkingInteractor = NetworkingInteractor(relayClient: relayClient, serializer: serializer, logger: logger, rpcHistory: history)
         let SIWEMessageFormatter = SIWEMessageFormatter()
         let appPairService = AppPairService(networkingInteractor: networkingInteractor, kms: kms, pairingStorage: pairingStore)
         let appRequestService = AppRequestService(networkingInteractor: networkingInteractor, kms: kms, appMetadata: metadata)
