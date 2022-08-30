87204814efce9f3925c0c114b920fd4d09c7234f
Bartosz Rozwarski <bartus000@gmail.com>
2022-08-30 12:18:47 +0200

    update api docs

--- Sources/Auth/Auth.swift
+++ Sources/Auth/Auth.swift
@@ -2,8 +2,20 @@ import Foundation
 import WalletConnectRelay
 import Combine
 
+/// Auth instatnce wrapper
+///
+/// ```swift
+/// let metadata = AppMetadata(
+///         name: "Swift wallet",
+///         description: "wallet",
+///         url: "wallet.connect",
+///         icons:  ["https://my_icon.com/1"])
+/// Auth.configure(metadata: metadata, account: account)
+/// try await Auth.instance.pair(uri: uri)
+/// ```
 public class Auth {
 
+    /// Auth client instance
     public static var instance: AuthClient = {
         guard let config = Auth.config else {
             fatalError("Error - you must call Auth.configure(_:) before accessing the shared instance.")
@@ -18,6 +30,10 @@ public class Auth {
 
     private init() { }
 
+    /// Auth instance config method
+    /// - Parameters:
+    ///   - metadata: App metadata
+    ///   - account: account that wallet will be authenticating with. Should be nil for non wallet clients.
     static public func configure(metadata: AppMetadata, account: Account?) {
         Auth.config = Auth.Config(
             metadata: metadata,
--- Sources/WalletConnectSign/Sign/Sign.swift
+++ Sources/WalletConnectSign/Sign/Sign.swift
@@ -6,8 +6,20 @@ import Combine
 public typealias Account = WalletConnectUtils.Account
 public typealias Blockchain = WalletConnectUtils.Blockchain
 
+/// Sign instatnce wrapper
+///
+/// ```swift
+/// let metadata = AppMetadata(
+///         name: "Swift wallet",
+///         description: "wallet",
+///         url: "wallet.connect",
+///         icons:  ["https://my_icon.com/1"])
+/// Sign.configure(metadata: metadata)
+/// try await Sign.instance.pair(uri: uri)
+/// ```
 public class Sign {
 
+    /// Sign client instance
     public static var instance: SignClient = {
         guard let metadata = Sign.metadata else {
             fatalError("Error - you must call Sign.configure(_:) before accessing the shared instance.")
@@ -22,6 +34,9 @@ public class Sign {
 
     private init() { }
 
+    /// Sign instance config method
+    /// - Parameters:
+    ///   - metadata: App metadata
     static public func configure(metadata: AppMetadata) {
         Sign.metadata = metadata
     }
