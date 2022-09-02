a54aafd863c2fc95da03f1874a93db89b6e066cb
Bartosz Rozwarski <bartus000@gmail.com>
2022-09-02 10:55:52 +0200

    Add disconnect method

--- Sources/Auth/Services/Common/DisconnectPairService.swift
+++ Sources/Auth/Services/Common/DisconnectPairService.swift
@@ -1,8 +1,31 @@
-//     
 
 import Foundation
+import WalletConnectNetworking
+import WalletConnectKMS
+import WalletConnectUtils
+import WalletConnectPairing
 
 class DisconnectPairService {
+    private let networkingInteractor: NetworkInteracting
+    private let kms: KeyManagementServiceProtocol
+    private let pairingStorage: WCPairingStorage
+    private let logger: ConsoleLogging
 
+    init(networkingInteractor: NetworkInteracting,
+         kms: KeyManagementServiceProtocol,
+         pairingStorage: WCPairingStorage,
+         logger: ConsoleLogging) {
+        self.networkingInteractor = networkingInteractor
+        self.kms = kms
+        self.pairingStorage = pairingStorage
+    }
 
+    func disconnect(topic: String) async throws {
+        let reason = AuthError.userDisconnected
+        logger.debug("Will delete pairing for reason: message: \(reason.message) code: \(reason.code)")
+        try await networkingInteractor.request(<#T##RPCRequest#>, topic: <#T##String#>, tag: <#T##Int#>, envelopeType: <#T##Envelope.EnvelopeType#>)
+        pairingStorage.delete(topic: topic)
+        kms.deleteSymmetricKey(for: topic)
+        networkingInteractor.unsubscribe(topic: topic)
+    }
 }
