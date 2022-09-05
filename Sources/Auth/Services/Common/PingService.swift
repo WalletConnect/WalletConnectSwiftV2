fe5222d678e22adb6bb0f17ad82dc6d345683c23
Bartosz Rozwarski <bartus000@gmail.com>
2022-09-05 11:28:48 +0200

    update ping service

--- Sources/Auth/Services/Common/PingService.swift
+++ Sources/Auth/Services/Common/PingService.swift
@@ -1,13 +1,21 @@
 import Foundation
 import WalletConnectPairing
 import JSONRPC
+import WalletConnectNetworking
 
 class PingService {
     private let pairingStorage: WCPairingStorage
     private let networkingInteractor: NetworkInteracting
 
-    func ping(topic: String, completion: @escaping (Result<Void, Error>) -> Void) {
-        guard pairingStorage.hasSession(forTopic: topic) else { return }
-        let request = RPCRequest(
+    init(pairingStorage: WCPairingStorage, networkingInteractor: NetworkInteracting) {
+        self.pairingStorage = pairingStorage
+        self.networkingInteractor = networkingInteractor
+    }
+
+    func ping(topic: String) async throws {
+        guard pairingStorage.hasPairing(forTopic: topic) else { return }
+        let request = RPCRequest(method: AuthProtocolMethods.pairingPing.rawValue, params: PairingPingParams())
+        try await networkingInteractor.request(request, topic: topic, tag: AuthProtocolMethods.pairingDelete.requestTag)
+        //todo return a publisher?
     }
 }
