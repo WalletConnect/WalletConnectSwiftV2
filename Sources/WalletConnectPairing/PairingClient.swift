f59f7bfb24305afd6de14e18d128de0e1b9a5222
Bartosz Rozwarski <bartus000@gmail.com>
2022-09-12 09:55:55 +0200

    savepoint

--- Sources/WalletConnectPairing/PairingClient.swift
+++ Sources/WalletConnectPairing/PairingClient.swift
@@ -1,6 +1,32 @@
 import Foundation
+import WalletConnectUtils
 
 class PairingClient {
+    private let walletPairService: WalletPairService
+    private let appPairService: AppPairService
 
-    
+
+    init(appPairService: AppPairService,
+         walletPairService: WalletPairService
+    ) {
+        self.appPairService = appPairService
+        self.walletPairService = walletPairService
+    }
+    /// For wallet to establish a pairing and receive an authentication request
+    /// Wallet should call this function in order to accept peer's pairing proposal and be able to subscribe for future authentication request.
+    /// - Parameter uri: Pairing URI that is commonly presented as a QR code by a dapp or delivered with universal linking.
+    ///
+    /// Throws Error:
+    /// - When URI is invalid format or missing params
+    /// - When topic is already in use
+    public func pair(uri: WalletConnectURI) async throws {
+        try await walletPairService.pair(uri)
+    }
+
+    public func create()  async throws -> WalletConnectURI {
+        return try await appPairService.create()
+    }
+
+    public func addSubscriber()
+
 }
