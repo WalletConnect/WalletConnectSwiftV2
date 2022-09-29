7db4cdf46aaacbce490c3ab3e5c59656aad66002
Bartosz Rozwarski <bartus000@gmail.com>
2022-09-29 12:56:04 +0200

    Fix bug with responding twice for pairing ping

--- Sources/WalletConnectPairing/PairingRequestsSubscriber.swift
+++ Sources/WalletConnectPairing/PairingRequestsSubscriber.swift
@@ -9,10 +9,14 @@ public class PairingRequestsSubscriber {
     private let pairingStorage: PairingStorage
     private var publishers = Set<AnyCancellable>()
     private var protocolMethods = SetStore<String>()
+    private let logger: ConsoleLogging
 
-    init(networkingInteractor: NetworkInteracting, pairingStorage: PairingStorage, logger: ConsoleLogging) {
+    init(networkingInteractor: NetworkInteracting,
+         pairingStorage: PairingStorage,
+         logger: ConsoleLogging) {
         self.networkingInteractor = networkingInteractor
         self.pairingStorage = pairingStorage
+        self.logger = logger
         handleUnregisteredRequests()
     }
 
@@ -30,11 +34,16 @@ public class PairingRequestsSubscriber {
     }
 
     func handleUnregisteredRequests() {
+        Task(priority: .high) { [unowned self] in
+            await protocolMethods.insert(PairingDeleteProtocolMethod().method)
+            await protocolMethods.insert(PairingPingProtocolMethod().method)
+        }
         networkingInteractor.requestPublisher
             .asyncFilter { [unowned self] in await !protocolMethods.contains($0.request.method)}
             .sink { [unowned self] topic, request in
                 Task(priority: .high) {
                     let protocolMethod = UnsupportedProtocolMethod(method: request.method)
+                    logger.debug("PairingRequestsSubscriber: responding unregistered request method")
                     try await networkingInteractor.respondError(topic: topic, requestId: request.id!, protocolMethod: protocolMethod, reason: PairError.methodUnsupported)
                 }
             }.store(in: &publishers)
