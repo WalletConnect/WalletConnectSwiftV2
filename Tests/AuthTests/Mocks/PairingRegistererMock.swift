df93845b9d3e822a2daff98e4b87bb257e3fd23b
Bartosz Rozwarski <bartus000@gmail.com>
2022-09-30 15:02:28 +0200

    fix unit tests

--- Tests/AuthTests/Mocks/PairingRegistererMock.swift
+++ Tests/AuthTests/Mocks/PairingRegistererMock.swift
@@ -4,9 +4,13 @@ import Combine
 import WalletConnectNetworking
 
 struct PairingRegistererMock<RequestParams>: PairingRegisterer where RequestParams : Codable {
+
     let subject = PassthroughSubject<RequestSubscriptionPayload<RequestParams>, Never>()
 
     func register<RequestParams>(method: ProtocolMethod) -> AnyPublisher<RequestSubscriptionPayload<RequestParams>, Never> where RequestParams : Decodable, RequestParams : Encodable {
         subject.eraseToAnyPublisher() as! AnyPublisher<RequestSubscriptionPayload<RequestParams>, Never>
+    }
+    
+    func validatePairingExistance(_ topic: String) throws {
     }
 }
