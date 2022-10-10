92014c24b1355afcf5dcf410dd9a28e4b20593db
Derek <alexanderderekrein@gmail.com>
2022-10-10 13:07:58 -0500

    feat: add push to sample wallet

--- Example/ExampleApp/AppDelegate.swift
+++ Example/ExampleApp/AppDelegate.swift
@@ -1,9 +1,21 @@
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        registerForPushNotifications()
+        
+        let notificationOption = launchOptions?[.remoteNotification]
+
+        // 1
+        if
+          let notification = notificationOption as? [String: AnyObject],
+          let aps = notification["aps"] as? [String: AnyObject] {
+          
+        }
+
         return true
     }
 
@@ -14,5 +26,77 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+    
+    func getNotificationSettings() {
+      UNUserNotificationCenter.current().getNotificationSettings { settings in
+        print("Notification settings: \(settings)")
+          guard settings.authorizationStatus == .authorized else { return }
+          DispatchQueue.main.async {
+            UIApplication.shared.registerForRemoteNotifications()
+          }
+      }
+    }
+
+    
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+          .requestAuthorization(
+            options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+            print("Permission granted: \(granted)")
+            guard granted else { return }
+            self?.getNotificationSettings()
+          }
+    }
+    
+    func application(
+      _ application: UIApplication,
+      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+      let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+      let token = tokenParts.joined()
+      print("Device Token: \(token)")
+        
+      let url = URL(string: "https://push.walletconnect.com")!
+        
+        // register in echo server
+    }
+
+    func application(
+      _ application: UIApplication,
+      didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+      print("Failed to register: \(error)")
+    }
+    
+    func registerClient(
+        clientId: String,
+        deviceToken: String,
+        body: Data,
+        then handler: @escaping (Result<Data, Error>) -> Void
+    ) {
+        var urlSession = URLSession.shared
+        // To ensure that our request is always sent, we tell
+        // the system to ignore all local cache data:
+        var request = URLRequest(
+            url: URL(string: "https://rpc.walletconnect.com/")!,
+            cachePolicy: .reloadIgnoringLocalCacheData
+        )
+        
+        let json: [String: Any] = ["client_id": clientId, "type": "APNS", "token": deviceToken]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        request.httpMethod = "POST"
+request.httpBody = jsonData
+
+        let task = urlSession.dataTask(
+            with: request,
+            completionHandler: { data, response, error in
+                // Validate response and call handler
+                print("Yeay")
+            }
+        )
+
+        task.resume()
     }
 }
