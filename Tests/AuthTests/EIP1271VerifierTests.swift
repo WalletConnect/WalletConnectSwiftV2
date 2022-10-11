af92e582498d007dcc02df7741abcaf003077be4
Artur Guseinov <arthur.guseinov@gmail.com>
2022-10-11 17:12:48 -0500

    Cleanup

--- Tests/AuthTests/EIP1271VerifierTests.swift
+++ Tests/AuthTests/EIP1271VerifierTests.swift
@@ -4,4 +4,15 @@ import XCTest
 
 class EIP1271VerifierTests: XCTestCase {
 
+//    private let verifier = EIP1271Verifier(projectId: "project-id", httpClient: HTTPClient)
+
+    func testVerify() async throws {
+    //        let client = HTTPClient(host: "rpc.walletconnect.com")
+    //        let service = EIP1271Verifier(projectId: "", httpClient: client)
+    //        try await service.verify(
+    //            signature: Data(hex: "c1505719b2504095116db01baaf276361efd3a73c28cf8cc28dabefa945b8d536011289ac0a3b048600c1e692ff173ca944246cf7ceb319ac2262d27b395c82b1c"),
+    //            messageHash: Data(hex: "3aaa8393796c7388e4e062861d8238503de7584c977676fe9d8d551c30e11f84"),
+    //            address: "0x2faf83c542b68f1b4cdc0e770e8cb9f567b08f71"
+    //        )
+    }
 }
--- Tests/AuthTests/SignerTests.swift
+++ Tests/AuthTests/SignerTests.swift
@@ -26,44 +26,6 @@ class SignerTest: XCTestCase {
             .data(using: .utf8)! + message
     }
 
-//    func testEtherscanSignature() async throws {
-//        let addressFromEtherscan = "0x6721591d424c18b7173d55895efa1839aa57d9c2"
-//        let message = "[Etherscan.io 12/08/2022 09:26:23] I, hereby verify that I am the owner/creator of the address [0x7e77dcb127f99ece88230a64db8d595f31f1b068]"
-//        let signedMessageFromEtherscan = message.data(using: .utf8)!
-//        let signatureHashFromEtherscan = Data(hex: "60eb9cfe362210f1b4855f4865eafc378bd442c406de22354cc9f643fb84cb265b7f6d9d10b13199e450558c328814a9038884d9993d9feb79b727366736853d1b")
-//        XCTAssertTrue(try signer.isValid(
-//            signature: signatureHashFromEtherscan,
-//            message: signedMessageFromEtherscan,
-//            address: addressFromEtherscan
-//        ))
-//
-//        let client = HTTPClient(host: "rpc.walletconnect.com")
-//        let service = EIP1271Verifier(projectId: "28fc11c6a12a4184bc8e9c371edff2bc", httpClient: client)
-//        try await service.verify(
-//            signature: Data(hex: "c1505719b2504095116db01baaf276361efd3a73c28cf8cc28dabefa945b8d536011289ac0a3b048600c1e692ff173ca944246cf7ceb319ac2262d27b395c82b1c"),
-//            messageHash: Data(hex: "3aaa8393796c7388e4e062861d8238503de7584c977676fe9d8d551c30e11f84"),
-//            address: "0x2faf83c542b68f1b4cdc0e770e8cb9f567b08f71"
-//        )
-//    }
-//
-//    func testInvalidMessage() throws {
-//        let message = "Message One".data(using: .utf8)!
-//
-//        XCTAssertFalse(try signer.isValid(signature: signature, message: message, address: address))
-//    }
-//
-//    func testInvalidPubKey() throws {
-//        let address = "0xBAc675C310721717Cd4A37F6cbeA1F081b1C2a07"
-//
-//        XCTAssertFalse(try signer.isValid(signature: signature, message: message, address: address))
-//    }
-//
-//    func testInvalidSignature() throws {
-//        let signature = Data(hex: "86deb09d045608f2753ef12f46e8da5fc2559e3a9162e580df3e62c875df7c3f64433462a59bc4ff38ce52412bff10527f4b99cc078f63ef2bb4a6f7427080aa01")
-//
-//        XCTAssertFalse(try signer.isValid(signature: signature, message: message, address: address))
-//    }
-
     func testSignerAddressFromIss() throws {
         let iss = "did:pkh:eip155:1:0xBAc675C310721717Cd4A37F6cbeA1F081b1C2a07"
 
