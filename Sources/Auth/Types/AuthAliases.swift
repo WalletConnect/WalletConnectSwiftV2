60b39eb75e4e63dbbe7a7dd0f320bb2f030f6c26
Artur Guseinov <arthur.guseinov@gmail.com>
2022-11-02 20:16:11 +0600

    File duplicates removed

---
+++ Sources/Auth/Types/AuthAliases.swift
@@ -0,0 +1,9 @@
+import JSONRPC
+import WalletConnectUtils
+import WalletConnectPairing
+
+public typealias RPCID = JSONRPC.RPCID
+public typealias AppMetadata = WalletConnectPairing.AppMetadata
+public typealias Account = WalletConnectUtils.Account
+public typealias Pairing = WalletConnectPairing.Pairing
+public typealias WalletConnectURI = WalletConnectUtils.WalletConnectURI
