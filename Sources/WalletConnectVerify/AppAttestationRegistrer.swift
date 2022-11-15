95d206a9d610b89d03efb739c9ca7662bed84f91
Bartosz Rozwarski <bartus000@gmail.com>
2022-11-15 09:49:17 +0100

    remove print statement

--- Sources/WalletConnectVerify/AppAttestationRegistrer.swift
+++ Sources/WalletConnectVerify/AppAttestationRegistrer.swift
@@ -31,7 +31,6 @@ class AppAttestationRegistrer {
 
     func registerAttestationIfNeeded() async throws {
         if let _ = try? keyIdStorage.get(key: keyIdStorageKey) { return }
-        print(keyIdStorage.getAll())
         let keyId = try await generateKeys()
         let challenge = try await getChallenge()
         let hash = Data(SHA256.hash(data: challenge))
