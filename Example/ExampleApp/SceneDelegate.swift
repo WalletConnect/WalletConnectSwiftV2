434882721083bc7df52ebdd357a3c39ef7dfa394
Artur Guseinov <arthur.guseinov@gmail.com>
2022-12-16 16:02:34 +0500

    ScenDelegate changes reverted

--- Example/ExampleApp/SceneDelegate.swift
+++ Example/ExampleApp/SceneDelegate.swift
@@ -10,7 +10,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) async {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         let metadata = AppMetadata(
             name: "Example Wallet",
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         Pair.configure(metadata: metadata)
 #if DEBUG
         if CommandLine.arguments.contains("-cleanInstall") {
-            try? await Sign.instance.cleanup()
+            try? Sign.instance.cleanup()
         }
 #endif
 
