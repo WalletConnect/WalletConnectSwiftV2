f43e75864136ba530c29a63eb822c41aeb532127
Artur Guseinov <arthur.guseinov@gmail.com>
2023-01-18 16:53:47 +0500

    Configure WebView in viewWillAppear

--- Example/Showcase/Classes/PresentationLayer/Web3Inbox/Web3InboxViewController.swift
+++ Example/Showcase/Classes/PresentationLayer/Web3Inbox/Web3InboxViewController.swift
@@ -4,8 +4,9 @@ import WebKit
 
 final class Web3InboxViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
         Web3Inbox.configure(account: Account("eip155:1:0x2F871A068a16BF4a970663dF5e417951aB79Bfd3")!)
         self.view = Web3Inbox.instance.getWebView()
     }
