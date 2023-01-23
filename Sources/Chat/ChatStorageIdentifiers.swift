fecec947a7392d395705de544b17f0ba3322c43d
Artur Guseinov <arthur.guseinov@gmail.com>
2023-01-23 14:54:04 +0530

    jsonRpcHistory key removed

--- Sources/Chat/ChatStorageIdentifiers.swift
+++ Sources/Chat/ChatStorageIdentifiers.swift
@@ -3,5 +3,4 @@ import Foundation
 enum ChatStorageIdentifiers: String {
     case topicToInvitationPubKey = "com.walletconnect.chat.topicToInvitationPubKey"
     case threads = "com.walletconnect.chat.threads"
-    case jsonRpcHistory = "com.walletconnect.chat.jsonRpcHistory"
 }
