7d52c5bb49cf36911569ac975bc98af3875b20c1
Bartosz Rozwarski <bartus000@gmail.com>
2023-02-02 12:03:47 +0100

    extract PushSubscriptionsObserver

--- Sources/WalletConnectPush/Client/Wallet/WalletPushClient.swift
+++ Sources/WalletConnectPush/Client/Wallet/WalletPushClient.swift
@@ -124,22 +124,3 @@ extension WalletPushClient {
 }
 #endif
 
-class PushSubscriptionsObserver {
-    public var subscriptionsPublisher: AnyPublisher<[PushSubscription], Never> {
-        subscriptionsPublisherSubject.eraseToAnyPublisher()
-    }
-    private let subscriptionsPublisherSubject = PassthroughSubject<[PushSubscription], Never>()
-
-    private let store: CodableStore<PushSubscription>
-    
-    init(store: CodableStore<PushSubscription>) {
-        self.store = store
-        setUpSubscription()
-    }
-
-    func setUpSubscription() {
-        store.onStoreUpdate = { [unowned self] in
-            subscriptionsPublisherSubject.send(store.getAll())
-        }
-    }
-}
