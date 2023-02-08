2e17fa49878d5230d06b126c5469d5c925074ee2
Artur Guseinov <arthur.guseinov@gmail.com>
2023-02-08 15:15:19 +0530

    MessageSignerMock fixed

--- Tests/AuthTests/Stubs/MessageSignerMock.swift
+++ Tests/AuthTests/Stubs/MessageSignerMock.swift
@@ -2,6 +2,9 @@ import Foundation
 import Auth
 
 struct MessageSignerMock: CacaoMessageSigner {
+    func sign(message: String, privateKey: Data, type: WalletConnectUtils.CacaoSignatureType) throws -> WalletConnectUtils.CacaoSignature {
+        return CacaoSignature(t: .eip191, s: "")
+    }
 
     func verify(signature: CacaoSignature,
         message: String,
