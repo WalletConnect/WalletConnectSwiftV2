e71e55ec5ba60cc83ff09d77e2e15b41577f054b
Bartosz Rozwarski <bartus000@gmail.com>
2023-02-15 11:39:14 +0100

    convert publishAt to date

--- Sources/WalletConnectRelay/RPC/Methods/Subscription.swift
+++ Sources/WalletConnectRelay/RPC/Methods/Subscription.swift
@@ -6,9 +6,28 @@ struct Subscription: RelayRPC {
 
     struct Params: Codable {
         struct Contents: Codable {
+
             let topic: String
             let message: String
-            let publishedAt: UInt64
+            let publishedAt: Date
+
+            enum CodingKeys: String, CodingKey {
+                case topic, message, publishedAt
+            }
+
+            internal init(topic: String, message: String, publishedAt: Date) {
+                self.topic = topic
+                self.message = message
+                self.publishedAt = publishedAt
+            }
+
+            init(from decoder: Decoder) throws {
+                let container = try decoder.container(keyedBy: CodingKeys.self)
+                topic = try container.decode(String.self, forKey: .topic)
+                message = try container.decode(String.self, forKey: .message)
+                let publishedAtMiliseconds = try container.decode(UInt64.self, forKey: .publishedAt)
+                publishedAt = Date(milliseconds: publishedAtMiliseconds)
+            }
         }
         let id: String
         let data: Contents
@@ -21,6 +40,6 @@ struct Subscription: RelayRPC {
     }
 
     init(id: String, topic: String, message: String) {
-        self.params = Params(id: id, data: Params.Contents(topic: topic, message: message, publishedAt: Date().millisecondsSince1970))
+        self.params = Params(id: id, data: Params.Contents(topic: topic, message: message, publishedAt: Date()))
     }
 }
--- Sources/WalletConnectUtils/Extensions/Date.swift
+++ Sources/WalletConnectUtils/Extensions/Date.swift
@@ -2,7 +2,11 @@
 import Foundation
 
 public extension Date {
-    public var millisecondsSince1970: UInt64 {
+    var millisecondsSince1970: UInt64 {
         UInt64((self.timeIntervalSince1970 * 1000.0).rounded())
+    }
+
+    init(milliseconds: UInt64) {
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
     }
 }
