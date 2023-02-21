e80987da8c434ca226d61137011577855dfb298a
Alexander Lisovik <lisovik.alexander@gmail.com>
2023-02-21 11:35:57 +0100

    Increase random range to 1000000

--- Sources/JSONRPC/RPCID.swift
+++ Sources/JSONRPC/RPCID.swift
@@ -10,7 +10,7 @@ struct IntIdentifierGenerator: IdentifierGenerator {
 
     func next() -> RPCID {
         let timestamp = Int64(Date().timeIntervalSince1970 * 1000 * pow(10, 6))
-        let random = Int64.random(in: 0..<1000)
+        let random = Int64.random(in: 0..<1000000)
         let extra = Int64(ceil(Float(random) * (pow(10, 6))))
         return RPCID(timestamp + extra)
     }
--- Sources/WalletConnectUtils/JsonRecID.swift
+++ Sources/WalletConnectUtils/JsonRecID.swift
@@ -4,7 +4,7 @@ public struct JsonRpcID {
 
     public static func generate() -> Int64 {
         let timestamp = Int64(Date().timeIntervalSince1970 * 1000 * pow(10, 6))
-        let random = Int64.random(in: 0..<1000)
+        let random = Int64.random(in: 0..<1000000)
         let extra = Int64(ceil(Float(random) * (pow(10, 6))))
         return timestamp + extra
     }
