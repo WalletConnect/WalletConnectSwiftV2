38506ff5838b703cf84b7a48112d4adc36a13385
Artur Guseinov <arthur.guseinov@gmail.com>
2023-03-15 16:12:42 +0500

    Integration tests fixed

--- Example/IntegrationTests/Auth/Signer/CacaoSignerTests.swift
+++ Example/IntegrationTests/Auth/Signer/CacaoSignerTests.swift
@@ -12,7 +12,7 @@ class CacaoSignerTest: XCTestCase {
     let message: String =
         """
         service.invalid wants you to sign in with your Ethereum account:
-        0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+        0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
 
         I accept the ServiceOrg Terms of Service: https://service.invalid/tos
 
