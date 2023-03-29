5dd6bf474f7b88c64f0324d9d311bb22c8356690
Alexander Lisovyk <lisovik.alexander@gmail.com>
2023-03-29 16:50:27 +0400

    Update client to js rpcid

--- Tests/WalletConnectSignTests/WCResponseTests.swift
+++ Tests/WalletConnectSignTests/WCResponseTests.swift
@@ -7,9 +7,9 @@ final class RPCIDTests: XCTestCase {
     func testTimestamp() {
         let request = RPCRequest(method: "method")
         let response = RPCResponse(matchingRequest: request, error: JSONRPCError(code: 0, message: "message"))
-        let timestamp = Date(timeIntervalSince1970: TimeInterval(request.id!.right! / 1000 / 1000000))
+        let timestamp = Date(timeIntervalSince1970: TimeInterval(request.id!.right! / 1000 / 1000))
         
-        XCTAssertEqual(response.id!.right!.description.count, 19)
+        XCTAssertEqual(response.id!.right!.description.count, 16)
         XCTAssertEqual(response.id!.timestamp, timestamp)
         XCTAssertTrue(Calendar.current.isDateInToday(response.id!.timestamp))
     }
