b4456a1ad19fcde1fedc62b8376118889dcf8305
Bartosz Rozwarski <bartus000@gmail.com>
2023-04-05 11:47:46 +0200

    remove decryption service files

--- Example/PNDecryptionService/NotificationService.swift
+++ Example/PNDecryptionService/NotificationService.swift
@@ -1,18 +1,24 @@
-
 import UserNotifications
 import WalletConnectPush
+import os
 
 class NotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
+    private static let logger = Logger(
+           subsystem: "com.walletconnect.walletapp.PNDecryptionService",
+           category: "NotificationService")
+
+    //com.walletconnect.walletapp.PNDecryptionService
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        if let bestAttemptContent = bestAttemptContent,
-           let topic = bestAttemptContent.userInfo["topic"] as? String,
-           let ciphertext = bestAttemptContent.userInfo["blob"] as? String {
+        Self.logger.trace("\("echo-test: Received notification from echo server", privacy: .public)")
+        if let bestAttemptContent = bestAttemptContent {
+            let topic = bestAttemptContent.userInfo["topic"] as! String
+            let ciphertext = bestAttemptContent.userInfo["blob"] as! String
             do {
                 let service = PushDecryptionService()
                 let pushMessage = try service.decryptMessage(topic: topic, ciphertext: ciphertext)
@@ -22,10 +28,12 @@ class NotificationService: UNNotificationServiceExtension {
                 return
             }
             catch {
-                print(error)
+                Self.logger.trace("\("echo-test: PN decryption error: \(error.localizedDescription)", privacy: .public)")
+
+                Self.logger.warning("\(error.localizedDescription, privacy: .public)")
+                bestAttemptContent.title = ""
+                bestAttemptContent.body = "content not set"
             }
-            bestAttemptContent.title = ""
-            bestAttemptContent.body = "content not set"
             contentHandler(bestAttemptContent)
         }
     }
