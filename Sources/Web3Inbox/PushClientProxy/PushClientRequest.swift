a3b6b69d3684cec34a2237677e5b038e969c6e98
Bartosz Rozwarski <bartus000@gmail.com>
2023-04-17 11:14:36 +0200

    Add PushClientRequestSubscriber

--- Sources/Web3Inbox/PushClientProxy/PushClientRequest.swift
+++ Sources/Web3Inbox/PushClientProxy/PushClientRequest.swift
@@ -1,3 +1,10 @@
-//     
-
 import Foundation
+
+enum PushClientRequest: String {
+    case pushRequest = "push_request"
+    case pushMessage = "push-message"
+
+    var method: String {
+        return rawValue
+    }
+}
--- Sources/Web3Inbox/PushClientProxy/PushClientRequestSubscriber.swift
+++ Sources/Web3Inbox/PushClientProxy/PushClientRequestSubscriber.swift
@@ -1,3 +1,52 @@
-//     
-
 import Foundation
+import Combine
+
+final class PushClientRequestSubscriber {
+
+    private var publishers: Set<AnyCancellable> = []
+
+    private let client: WalletPushClient
+    private let logger: ConsoleLogging
+
+    var onRequest: ((RPCRequest) async throws -> Void)?
+
+    init(client: WalletPushClient, logger: ConsoleLogging) {
+        self.client = client
+        self.logger = logger
+
+        setupSubscriptions()
+    }
+
+    func setupSubscriptions() {
+        client.requestPublisher.sink { [unowned self] id, account, metadata in
+            let params = RequestPayload(id: id, account: account, metadata: metadata)
+            handle(event: .pushRequest, params: params)
+        }.store(in: &publishers)
+        client.pushMessagePublisher.sink { [unowned self] record in
+            handle(event: .pushMessage, params: record)
+        }.store(in: &publishers)
+    }
+}
+
+private extension PushClientRequestSubscriber {
+
+    struct RequestPayload: Codable {
+        let id: RPCID
+        let account: Account
+        let metadata: AppMetadata
+    }
+
+    func handle(event: PushClientRequest, params: Codable) {
+        Task {
+            do {
+                let request = RPCRequest(
+                    method: event.method,
+                    params: params
+                )
+                try await onRequest?(request)
+            } catch {
+                logger.error("Client Request error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
