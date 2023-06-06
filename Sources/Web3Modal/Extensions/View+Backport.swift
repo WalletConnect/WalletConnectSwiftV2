697babd6c239dca70e1a37b750a180ce29d233cd
Radek Novak <radek.novis@gmail.com>
2023-06-06 14:28:30 +0200

    Delete unused files

--- Sources/Web3Modal/Extensions/View+Backport.swift
+++ Sources/Web3Modal/Extensions/View+Backport.swift
@@ -1,37 +1,5 @@
-import SwiftUI
 import Combine
-
-struct Backport<Content> {
-    let content: Content
-}
-
-extension View {
-    var backport: Backport<Self> { Backport(content: self) }
-}
-
-extension Backport where Content: View {
-    
-    enum Visibility {
-        case automatic
-        case visible
-        case hidden
-    }
-    
-    @ViewBuilder func scrollContentBackground(_ visibility: Backport.Visibility) -> some View {
-        if #available(iOS 16, *) {
-            switch visibility {
-            case .automatic:
-                content.scrollContentBackground(.automatic)
-            case .hidden:
-                content.scrollContentBackground(.hidden)
-            case .visible:
-                content.scrollContentBackground(.visible)
-            }
-        } else {
-            content
-        }
-    }
-}
+import SwiftUI
 
 extension View {
     /// A backwards compatible wrapper for iOS 14 `onChange`
@@ -39,11 +7,9 @@ extension View {
         if #available(iOS 14.0, *) {
             self.onChange(of: value, perform: perform)
         } else {
-            self.onReceive(Just(value)) { (value) in
+            self.onReceive(Just(value)) { value in
                 perform(value)
             }
         }
     }
 }
-
-
