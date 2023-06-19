5c2bbeb20c7eb43fe41a7d7d52010b5703277736
Bartosz Rozwarski <bartus000@gmail.com>
2023-06-19 13:17:24 +0200

    remove unused error

--- Sources/WalletConnectPush/Types/PushError.swift
+++ Sources/WalletConnectPush/Types/PushError.swift
@@ -3,7 +3,6 @@ import Foundation
 public enum PushError: Codable, Equatable, Error {
     case rejected
     case userHasExistingSubscription
-    case methodUnsupported
 }
 
 extension PushError: Reason {
@@ -12,16 +11,12 @@ extension PushError: Reason {
         switch code {
         case Self.rejected.code:
             self = .rejected
-        case Self.methodUnsupported.code:
-            self = .methodUnsupported
         default:
             return nil
         }
     }
     public var code: Int {
         switch self {
-        case .methodUnsupported:
-            return 10001
         case .rejected:
             return 15000
         case .userHasExistingSubscription:
@@ -33,8 +28,6 @@ extension PushError: Reason {
         switch self {
         case .rejected:
             return "Push request rejected"
-        case .methodUnsupported:
-            return "Method Unsupported"
         case .userHasExistingSubscription:
             return "User Has Existing Subscription"
         }
