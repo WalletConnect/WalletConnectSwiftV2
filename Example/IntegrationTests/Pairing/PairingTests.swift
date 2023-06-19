5785e2f9cfaa3034e9a448ad88cef6d71dc6cb21
Bartosz Rozwarski <bartus000@gmail.com>
2023-06-19 13:40:34 +0200

    savepoint

--- Example/IntegrationTests/Pairing/PairingTests.swift
+++ Example/IntegrationTests/Pairing/PairingTests.swift
@@ -133,7 +133,7 @@ final class PairingTests: XCTestCase {
         let expectation = expectation(description: "wallet responds unsupported method for unregistered method")
 
         appPushClient.proposalResponsePublisher.sink { (response) in
-            XCTAssertEqual(response, .failure(PushError(code: 10001)!))
+            XCTAssertEqual(response, .failure(PushError(code: 5000)!))
             expectation.fulfill()
         }.store(in: &publishers)
 
