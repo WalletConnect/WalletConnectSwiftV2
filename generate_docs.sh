9c6d61ef64a639104cd2794bfbc596aaaea69e90
Radek Novak <radek.novis@gmail.com>
2023-06-29 12:22:24 +0200

    Add documentation for all targets

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,14 +1,46 @@
-swift package --allow-writing-to-directory ./docs  \
-    generate-documentation \
-    --target WalletConnectModal \
-    --disable-indexing \
-    --include-extended-types \
-    --output-path ./docs \
-    --hosting-base-path WalletConnectSwiftV2 \
+#!/bin/bash
+
+DOCS_BASE_DIR="./docs"
+
+dump=$(swift package dump-package | jq -r '.products[].targets[]')
+
+IFS=$'\n' read -rd '' -a TARGETS <<<"$dump"
+
+mkdir -p tmp-doc
+
+for target in "${TARGETS[@]}"
+do
+    swift package --allow-writing-to-directory "./tmp-doc/${target}-docs"  \
+        generate-documentation \
+        --target $target \
+        --disable-indexing \
+        --include-extended-types \
+        --output-path "./tmp-doc/${target}-docs" \
+        --hosting-base-path WalletConnectSwiftV2
+done 
+
+rm -rf ${DOCS_BASE_DIR}
+is_first=1
+for target in "${TARGETS[@]}"
+do
+	if [ $is_first -eq 1 ]; then
+        echo "Copying initial documentation for ${target}"
+        cp -R "tmp-doc/${target}-docs" "${DOCS_BASE_DIR}"
+        is_first=0
+    else
+        echo "Merging documentation for ${target}"
+        cp -R "tmp-doc/${target}-docs/data/documentation/"* "${DOCS_BASE_DIR}/data/documentation/"
+        cp -R "tmp-doc/${target}-docs/documentation/"* "${DOCS_BASE_DIR}/documentation/"
+    fi
+done
+
+rm -rf tmp-doc 
 
 
-# xcodebuild docbuild \
-#     -scheme WalletConnectModal \
-#     -derivedDataPath ./derivedDataCache \              
-#     -destination 'platform=iOS Simulator,name=iPhone 13'
-
+# swift package --allow-writing-to-directory ./docs  \
+#     generate-documentation \
+#     --product WalletConnect \
+#     --disable-indexing \
+#     --include-extended-types \
+#     --output-path ./docs \
+#     --hosting-base-path WalletConnectSwiftV2 \
