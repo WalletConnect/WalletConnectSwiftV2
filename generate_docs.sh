bc2a80e944114f696ab77d25d5f37787e2dec0a4
Radek Novak <radek.novis@gmail.com>
2023-06-29 14:30:09 +0200

    Style the index slightly

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-DOCS_BASE_DIR="./docs"
+# DOCS_BASE_DIR="./docs"
 
-dump=$(swift package dump-package | jq -r '.products[].targets[]')
+# dump=$(swift package dump-package | jq -r '.products[].targets[]')
 
-IFS=$'\n' read -rd '' -a TARGETS <<<"$dump"
+# IFS=$'\n' read -rd '' -a TARGETS <<<"$dump"
 
-mkdir -p tmp-doc
+# mkdir -p tmp-doc
 
-for target in "${TARGETS[@]}"; do
-    swift package --allow-writing-to-directory "./tmp-doc/${target}-docs" \
-        generate-documentation \
-        --target $target \
-        --disable-indexing \
-        --include-extended-types \
-        --output-path "./tmp-doc/${target}-docs" \
-        --hosting-base-path WalletConnectSwiftV2
-done
+# for target in "${TARGETS[@]}"; do
+#     swift package --allow-writing-to-directory "./tmp-doc/${target}-docs" \
+#         generate-documentation \
+#         --target $target \
+#         --disable-indexing \
+#         --include-extended-types \
+#         --output-path "./tmp-doc/${target}-docs" \
+#         --hosting-base-path WalletConnectSwiftV2
+# done
 
-rm -rf ${DOCS_BASE_DIR}
-is_first=1
-for target in "${TARGETS[@]}"; do
-    if [ $is_first -eq 1 ]; then
-        echo "Copying initial documentation for ${target}"
-        cp -R "tmp-doc/${target}-docs" "${DOCS_BASE_DIR}"
-        is_first=0
-    else
-        echo "Merging documentation for ${target}"
-        cp -R "tmp-doc/${target}-docs/data/documentation/"* "${DOCS_BASE_DIR}/data/documentation/"
-        cp -R "tmp-doc/${target}-docs/documentation/"* "${DOCS_BASE_DIR}/documentation/"
-    fi
-done
-echo "Deleting non-mergable metadata.json"
-rm -f "${DOCS_BASE_DIR}/metadata.json"
-rm -rf tmp-doc
+# rm -rf ${DOCS_BASE_DIR}
+# is_first=1
+# for target in "${TARGETS[@]}"; do
+#     if [ $is_first -eq 1 ]; then
+#         echo "Copying initial documentation for ${target}"
+#         cp -R "tmp-doc/${target}-docs" "${DOCS_BASE_DIR}"
+#         is_first=0
+#     else
+#         echo "Merging documentation for ${target}"
+#         cp -R "tmp-doc/${target}-docs/data/documentation/"* "${DOCS_BASE_DIR}/data/documentation/"
+#         cp -R "tmp-doc/${target}-docs/documentation/"* "${DOCS_BASE_DIR}/documentation/"
+#     fi
+# done
+# echo "Deleting non-mergable metadata.json"
+# rm -f "${DOCS_BASE_DIR}/metadata.json"
+# rm -rf tmp-doc
 
 TARGET_DOCS_DIR='./docs/documentation'
 INDEX_FILE='./docs/index.html'
@@ -56,9 +56,45 @@ if [ ${target_count} -gt 1 ]; then
 <!DOCTYPE html>
 <html>
     <head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+            background-color: #d2d2d2;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        li {
+            margin-bottom: 10px;
+        }
+
+        a {
+            display: block;
+            padding: 10px;
+            background-color: #ccc;
+            text-decoration: none;
+            color: #337ab7;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+
+        a:hover {
+            background-color: #aaa;
+        }
+    </style>
         <title>${REPO_NAME} Documentation</title>
     </head>
     <body>
+        <h1>${REPO_NAME} Documentation</h1>
         <ul>
         ${target_list}
         </ul>
