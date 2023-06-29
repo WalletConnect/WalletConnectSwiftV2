e0e9f0c5306faa9162a08a95eedf409abe20a31f
Radek Novak <radek.novis@gmail.com>
2023-06-29 14:17:04 +0200

    Create index file to navigate to individual targets

--- generate_docs.sh
+++ generate_docs.sh
@@ -8,22 +8,20 @@ IFS=$'\n' read -rd '' -a TARGETS <<<"$dump"
 
 mkdir -p tmp-doc
 
-for target in "${TARGETS[@]}"
-do
-    swift package --allow-writing-to-directory "./tmp-doc/${target}-docs"  \
+for target in "${TARGETS[@]}"; do
+    swift package --allow-writing-to-directory "./tmp-doc/${target}-docs" \
         generate-documentation \
         --target $target \
         --disable-indexing \
         --include-extended-types \
         --output-path "./tmp-doc/${target}-docs" \
         --hosting-base-path WalletConnectSwiftV2
-done 
+done
 
 rm -rf ${DOCS_BASE_DIR}
 is_first=1
-for target in "${TARGETS[@]}"
-do
-	if [ $is_first -eq 1 ]; then
+for target in "${TARGETS[@]}"; do
+    if [ $is_first -eq 1 ]; then
         echo "Copying initial documentation for ${target}"
         cp -R "tmp-doc/${target}-docs" "${DOCS_BASE_DIR}"
         is_first=0
@@ -33,14 +31,52 @@ do
         cp -R "tmp-doc/${target}-docs/documentation/"* "${DOCS_BASE_DIR}/documentation/"
     fi
 done
+echo "Deleting non-mergable metadata.json"
+rm -f "${DOCS_BASE_DIR}/metadata.json"
+rm -rf tmp-doc
 
-rm -rf tmp-doc 
+TARGET_DOCS_DIR='./docs/documentation'
+INDEX_FILE='./docs/index.html'
+BASE_URL='https://walletconnect.github.io/WalletConnectSwiftV2/documentation'
+REPO_NAME='WalletConnectSwiftV2'
 
-
-# swift package --allow-writing-to-directory ./docs  \
-#     generate-documentation \
-#     --product WalletConnect \
-#     --disable-indexing \
-#     --include-extended-types \
-#     --output-path ./docs \
-#     --hosting-base-path WalletConnectSwiftV2 \
+target_count=0
+target_list=""
+single_target_name=""
+for target in $(ls "${TARGET_DOCS_DIR}"); do
+    if [ -d "${TARGET_DOCS_DIR}/${target}" ]; then
+        single_target_name="${target}"
+        target_count=$((target_count + 1))
+        target_list="${target_list}<li><a href=\"${BASE_URL}/${target}\"><code>${target}</code> Documentation</a></li>"
+    fi
+done
+if [ ${target_count} -gt 1 ]; then
+    echo "Found ${target_count} targets. Generating list..."
+    cat >"${INDEX_FILE}" <<EOF
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>${REPO_NAME} Documentation</title>
+    </head>
+    <body>
+        <ul>
+        ${target_list}
+        </ul>
+    </body>
+</html>
+EOF
+else
+    echo "Found one target. Generating redirect file to target ${single_target_name}"
+    cat >"${INDEX_FILE}" <<EOF
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>${REPO_NAME} Documentation</title>
+        <meta http-equiv="refresh" content="0; url=${BASE_URL}/${single_target_name}" />
+    </head>
+    <body>
+        <p>Redirecting...</p>
+    </body>
+</html>
+EOF
+fi
