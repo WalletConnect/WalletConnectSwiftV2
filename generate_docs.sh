530a2981d3040f2dd620b9e515fd7145500a9ab8
Radek Novak <radek.novis@gmail.com>
2023-06-29 16:28:53 +0200

    Add all targets rather than products

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-# DOCS_BASE_DIR="./docs"
+DOCS_BASE_DIR="./docs"
 
-# dump=$(swift package dump-package | jq -r '.products[].targets[]')
+dump=$(swift package dump-package | jq -r '.targets[].name')
 
-# IFS=$'\n' read -rd '' -a TARGETS <<<"$dump"
+IFS=$'\n' read -rd '' -a TARGETS <<<"$dump"
 
-# mkdir -p tmp-doc
+mkdir -p tmp-doc
 
-# for target in "${TARGETS[@]}"; do
-#     swift package --allow-writing-to-directory "./tmp-doc/${target}-docs" \
-#         generate-documentation \
-#         --target $target \
-#         --disable-indexing \
-#         --include-extended-types \
-#         --output-path "./tmp-doc/${target}-docs" \
-#         --hosting-base-path WalletConnectSwiftV2
-# done
+for target in "${TARGETS[@]}"; do
+    swift package --allow-writing-to-directory "./tmp-doc/${target}-docs" \
+        generate-documentation \
+        --target $target \
+        --disable-indexing \
+        --include-extended-types \
+        --output-path "./tmp-doc/${target}-docs" \
+        --hosting-base-path WalletConnectSwiftV2
+done
 
-# rm -rf ${DOCS_BASE_DIR}
-# is_first=1
-# for target in "${TARGETS[@]}"; do
-#     if [ $is_first -eq 1 ]; then
-#         echo "Copying initial documentation for ${target}"
-#         cp -R "tmp-doc/${target}-docs" "${DOCS_BASE_DIR}"
-#         is_first=0
-#     else
-#         echo "Merging documentation for ${target}"
-#         cp -R "tmp-doc/${target}-docs/data/documentation/"* "${DOCS_BASE_DIR}/data/documentation/"
-#         cp -R "tmp-doc/${target}-docs/documentation/"* "${DOCS_BASE_DIR}/documentation/"
-#     fi
-# done
-# echo "Deleting non-mergable metadata.json"
-# rm -f "${DOCS_BASE_DIR}/metadata.json"
-# rm -rf tmp-doc
+rm -rf ${DOCS_BASE_DIR}
+is_first=1
+for target in "${TARGETS[@]}"; do
+    if [ $is_first -eq 1 ]; then
+        echo "Copying initial documentation for ${target}"
+        cp -R "tmp-doc/${target}-docs" "${DOCS_BASE_DIR}"
+        is_first=0
+    else
+        echo "Merging documentation for ${target}"
+        cp -R "tmp-doc/${target}-docs/data/documentation/"* "${DOCS_BASE_DIR}/data/documentation/"
+        cp -R "tmp-doc/${target}-docs/documentation/"* "${DOCS_BASE_DIR}/documentation/"
+    fi
+done
+echo "Deleting non-mergable metadata.json"
+rm -f "${DOCS_BASE_DIR}/metadata.json"
+rm -rf tmp-doc
 
 TARGET_DOCS_DIR='./docs/documentation'
 INDEX_FILE='./docs/index.html'
