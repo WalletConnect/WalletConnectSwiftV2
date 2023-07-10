dadba997397bfe86798bdfbd3fabf668d4e8fda7
Alexander Lisovik <lisovik.alexander@gmail.com>
2023-07-10 21:23:41 +0200

    Fix merge conflicts

--- Example/WalletApp/ApplicationLayer/Configurator/ThirdPartyConfigurator.swift
+++ Example/WalletApp/ApplicationLayer/Configurator/ThirdPartyConfigurator.swift
@@ -3,14 +3,6 @@ import Foundation
 struct ThirdPartyConfigurator: Configurator {
 
     func configure() {
-        Networking.configure(projectId: InputConfig.projectId, socketFactory: DefaultSocketFactory())
-
-        let metadata = AppMetadata(
-            name: "Example Wallet",
-            description: "wallet description",
-            url: "example.wallet",
-            icons: ["https://avatars.githubusercontent.com/u/37784886"]
-        )
         
     }
 }
