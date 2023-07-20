559d25ffa23630d63f2ddcfd0ccaad26ff8ee9d1
Alexander Lisovik <lisovik.alexander@gmail.com>
2023-07-20 10:21:05 +0200

    Downgrade iOS version to 13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "WalletConnect",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v13),
         .macOS(.v12),
         .tvOS(.v14)
     ],
