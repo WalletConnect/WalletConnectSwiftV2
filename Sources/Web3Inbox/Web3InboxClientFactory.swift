76aed93131e03d1f1b88f1b160db5596dca6840d
Bartosz Rozwarski <bartus000@gmail.com>
2023-08-01 08:31:36 +0200

    update w3i url for notify

--- Sources/Web3Inbox/Web3InboxClientFactory.swift
+++ Sources/Web3Inbox/Web3InboxClientFactory.swift
@@ -40,7 +40,7 @@ final class Web3InboxClientFactory {
     }
 
     private static func buildUrl(account: Account, config: [ConfigParam: Bool]) -> URL {
-        var urlComponents = URLComponents(string: "https://web3inbox-dev-hidden.vercel.app/")!
+        var urlComponents = URLComponents(string: "https://web3inbox-dev-hidden-git-chore-filter-by-notify-walletconnect1.vercel.app/")!
         var queryItems = [URLQueryItem(name: "chatProvider", value: "ios"), URLQueryItem(name: "notifyProvider", value: "ios"), URLQueryItem(name: "account", value: account.address), URLQueryItem(name: "authProvider", value: "ios")]
 
         for param in config.filter({ $0.value == false}) {
