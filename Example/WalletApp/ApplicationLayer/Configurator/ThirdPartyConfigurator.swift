510e9dacff4c5ddd2ab981eb9d03fbeee0f1402b
Bartosz Rozwarski <bartus000@gmail.com>
2023-08-10 10:03:11 +0200

    disable debug mode

--- Example/WalletApp/ApplicationLayer/Configurator/ThirdPartyConfigurator.swift
+++ Example/WalletApp/ApplicationLayer/Configurator/ThirdPartyConfigurator.swift
@@ -11,7 +11,6 @@ struct ThirdPartyConfigurator: Configurator {
         guard let sentryDns = InputConfig.sentryDns else { return }
         SentrySDK.start { options in
             options.dsn = "https://\(sentryDns)"
-            options.debug = true // Enabled debug when first installing is always helpful
             // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
             // We recommend adjusting this value in production.
             options.tracesSampleRate = 1.0
