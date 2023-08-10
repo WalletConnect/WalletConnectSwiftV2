ba82d2aa10de9bb9499ef11aa9c6f10097bc3967
Bartosz Rozwarski <bartus000@gmail.com>
2023-08-10 08:23:38 +0200

    update sentry dns url

--- Example/WalletApp/ApplicationLayer/Configurator/ThirdPartyConfigurator.swift
+++ Example/WalletApp/ApplicationLayer/Configurator/ThirdPartyConfigurator.swift
@@ -5,7 +5,7 @@ struct ThirdPartyConfigurator: Configurator {
 
     func configure() {
         SentrySDK.start { options in
-            options.dsn = InputConfig.sentryDns
+            options.dsn = "https://\(InputConfig.sentryDns)"
             options.debug = true // Enabled debug when first installing is always helpful
             // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
             // We recommend adjusting this value in production.
