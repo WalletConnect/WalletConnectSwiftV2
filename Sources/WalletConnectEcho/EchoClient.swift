956bdd2ad58cd92ef566573ade83a7a7fb8b5aa1
Alexander Lisovik <lisovik.alexander@gmail.com>
2023-08-14 16:36:28 +0400

    Move fallback to register service

--- Sources/WalletConnectEcho/EchoClient.swift
+++ Sources/WalletConnectEcho/EchoClient.swift
@@ -3,39 +3,17 @@ import Foundation
 public class EchoClient: EchoClientProtocol {
     private let registerService: EchoRegisterService
 
-    /// The property is used to determine whether echo.walletconnect.org will be used
-    /// in case echo.walletconnect.com doesn't respond for some reason (most likely due to being blocked in the user's location).
-    private var fallback = false
-    
     init(registerService: EchoRegisterService) {
         self.registerService = registerService
     }
 
     public func register(deviceToken: Data) async throws {
-        do {
-            try await registerService.register(deviceToken: deviceToken)
-        } catch {
-            if (error as? HTTPError) == .couldNotConnect && !fallback {
-                fallback = true
-                await registerService.echoHostFallback()
-                try await registerService.register(deviceToken: deviceToken)
-            }
-            throw error
-        }
+        try await registerService.register(deviceToken: deviceToken)
     }
 
 #if DEBUG
     public func register(deviceToken: String) async throws {
-        do {
-            try await registerService.register(deviceToken: deviceToken)
-        } catch {
-            if (error as? HTTPError) == .couldNotConnect && !fallback {
-                fallback = true
-                await registerService.echoHostFallback()
-                try await registerService.register(deviceToken: deviceToken)
-            }
-            throw error
-        }
+        try await registerService.register(deviceToken: deviceToken)
     }
 #endif
 }
