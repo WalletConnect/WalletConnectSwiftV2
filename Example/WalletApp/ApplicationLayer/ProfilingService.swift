304342f0a026780ddb5f0fcfd6938ec8d0038d74
Bartosz Rozwarski <bartus000@gmail.com>
2023-08-27 17:02:21 +0100

    savepoint

--- Example/WalletApp/ApplicationLayer/ProfilingService.swift
+++ Example/WalletApp/ApplicationLayer/ProfilingService.swift
@@ -5,7 +5,8 @@ final class ProfilingService {
     public static var instance = ProfilingService()
 
     func setUpProfiling(account: String, clientId: String) {
-        Mixpanel.initialize(token: "", trackAutomaticEvents: true)
+        guard let token = InputConfig.mixpanelToken, !token.isEmpty  else { return }
+        Mixpanel.initialize(token: token, trackAutomaticEvents: true)
         Mixpanel.mainInstance().identify(distinctId: clientId)
         Mixpanel.mainInstance().people.set(properties: [ "account": account])
     }
