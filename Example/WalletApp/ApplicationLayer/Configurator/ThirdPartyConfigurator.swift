fceafe55e9b308ba5e756e46aa0fea182e59d422
Bartosz Rozwarski <bartus000@gmail.com>
2023-09-01 10:14:18 +0100

    update logging service

--- Example/WalletApp/ApplicationLayer/Configurator/ThirdPartyConfigurator.swift
+++ Example/WalletApp/ApplicationLayer/Configurator/ThirdPartyConfigurator.swift
@@ -7,12 +7,6 @@ struct ThirdPartyConfigurator: Configurator {
     }
 
     private func configureLogging() {
-        guard let sentryDsn = InputConfig.sentryDsn, !sentryDsn.isEmpty  else { return }
-        SentrySDK.start { options in
-            options.dsn = "https://\(sentryDsn)"
-            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-            // We recommend adjusting this value in production.
-            options.tracesSampleRate = 1.0
-        }
+        LoggingService.instance.configure()
     }
 }
--- Example/WalletApp/ApplicationLayer/LoggingService.swift
+++ Example/WalletApp/ApplicationLayer/LoggingService.swift
@@ -20,6 +20,16 @@ final class LoggingService {
         SentrySDK.setUser(user)
     }
 
+    func configure() {
+        guard let sentryDsn = InputConfig.sentryDsn, !sentryDsn.isEmpty  else { return }
+        SentrySDK.start { options in
+            options.dsn = "https://\(sentryDsn)"
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+        }
+    }
+
     func startLogging() {
         queue.sync {
             guard isLogging == false else { return }
