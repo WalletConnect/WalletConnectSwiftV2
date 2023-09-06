479ea6684efd8315cff2de5bae70d1ae724a5122
Bartosz Rozwarski <bartus000@gmail.com>
2023-09-06 18:34:28 +0200

    savepoint

--- Sources/WalletConnectNotify/Client/Wallet/ProtocolEngine/wc_notifyWatchSubscriptions/NotifyWatchSubscriptionsResponseSubscriber.swift
+++ Sources/WalletConnectNotify/Client/Wallet/ProtocolEngine/wc_notifyWatchSubscriptions/NotifyWatchSubscriptionsResponseSubscriber.swift
@@ -1,2 +1,39 @@
 import Foundation
+import Combine
 
+class NotifyWatchSubscriptionsResponseSubscriber {
+    private let networkingInteractor: NetworkInteracting
+    private let kms: KeyManagementServiceProtocol
+    private var publishers = [AnyCancellable]()
+    private let logger: ConsoleLogging
+    private let notifyStorage: NotifyStorage
+    private let subscriptionScopeProvider: SubscriptionScopeProvider
+
+    init(networkingInteractor: NetworkInteracting,
+         kms: KeyManagementServiceProtocol,
+         logger: ConsoleLogging,
+         notifyStorage: NotifyStorage,
+         subscriptionScopeProvider: SubscriptionScopeProvider
+    ) {
+        self.networkingInteractor = networkingInteractor
+        self.kms = kms
+        self.logger = logger
+        self.notifyStorage = notifyStorage
+        self.subscriptionScopeProvider = subscriptionScopeProvider
+        subscribeForWatchSubscriptionsResponse()
+    }
+
+
+    private func subscribeForWatchSubscriptionsResponse() {
+
+        let protocolMethod = NotifySubscribeProtocolMethod()
+        networkingInteractor.responseSubscription(on: protocolMethod)
+            .sink { [unowned self] (payload: ResponseSubscriptionPayload<NotifySubscriptionPayload.Wrapper, NotifySubscriptionResponsePayload.Wrapper>) in
+                Task(priority: .high) {
+                    logger.debug("Received Notify watch Subscription response")
+
+                    
+                }
+            }.store(in: &publishers)
+    }
+}
