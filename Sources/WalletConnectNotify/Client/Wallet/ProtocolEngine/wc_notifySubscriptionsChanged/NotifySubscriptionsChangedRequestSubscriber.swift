c37df34b6d9bfbdbcd55e95c502216f144171901
Bartosz Rozwarski <bartus000@gmail.com>
2023-09-06 23:21:32 +0200

    savepoint

---
+++ Sources/WalletConnectNotify/Client/Wallet/ProtocolEngine/wc_notifySubscriptionsChanged/NotifySubscriptionsChangedRequestSubscriber.swift
@@ -0,0 +1,31 @@
+import Foundation
+import Combine
+
+class NotifySubscriptionsChangedRequestSubscriber {
+
+    private let networkingInteractor: NetworkInteracting
+    private let kms: KeyManagementServiceProtocol
+    private var publishers = [AnyCancellable]()
+    private let logger: ConsoleLogging
+    private let notifyStorage: NotifyStorage
+    private let subscriptionScopeProvider: SubscriptionScopeProvider
+
+    init(networkingInteractor: NetworkInteracting,
+         kms: KeyManagementServiceProtocol,
+         logger: ConsoleLogging,
+         notifyStorage: NotifyStorage,
+         subscriptionScopeProvider: SubscriptionScopeProvider
+    ) {
+        self.networkingInteractor = networkingInteractor
+        self.kms = kms
+        self.logger = logger
+        self.notifyStorage = notifyStorage
+        self.subscriptionScopeProvider = subscriptionScopeProvider
+        subscribeForNofifyChangedRequests()
+    }
+
+
+    private func subscribeForNofifyChangedRequests() {
+
+    }
+}
--- Sources/WalletConnectNotify/Client/Wallet/SubscriptionScopeProvider.swift
+++ Sources/WalletConnectNotify/Client/Wallet/SubscriptionScopeProvider.swift
@@ -12,11 +12,22 @@ class SubscriptionScopeProvider {
         if let availableScope = cache[dappUrl] {
             return availableScope
         }
-        guard let scopeUrl = URL(string: "\(dappUrl)/.well-known/wc-notify-config.json") else { throw Errors.invalidUrl }
-        let (data, _) = try await URLSession.shared.data(from: scopeUrl)
+        guard let notifyConfigUrl = URL(string: "\(dappUrl)/.well-known/wc-notify-config.json") else { throw Errors.invalidUrl }
+        let (data, _) = try await URLSession.shared.data(from: notifyConfigUrl)
         let config = try JSONDecoder().decode(NotificationConfig.self, from: data)
         let availableScope = Set(config.types)
         cache[dappUrl] = availableScope
         return availableScope
+    }
+
+    func getMetadata(dappUrl: String) async throws -> AppMetadata {
+        guard let notifyConfigUrl = URL(string: "\(dappUrl)/.well-known/wc-notify-config.json") else { throw Errors.invalidUrl }
+        let (data, _) = try await URLSession.shared.data(from: notifyConfigUrl)
+        let config = try JSONDecoder().decode(NotificationConfig.self, from: data)
+
+        fatalError("  Can we add metadata to config?")
+
+        
+        return config.metadata
     }
 }
