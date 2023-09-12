73e56d5951a879da72998d03850872508d5f34cd
Bartosz Rozwarski <bartus000@gmail.com>
2023-09-12 12:04:44 +0200

    add group keychain to watch subscription

--- Sources/WalletConnectNotify/Client/Wallet/ProtocolEngine/wc_notifySubscriptionsChanged/NotifySubscriptionsChangedRequestSubscriber.swift
+++ Sources/WalletConnectNotify/Client/Wallet/ProtocolEngine/wc_notifySubscriptionsChanged/NotifySubscriptionsChangedRequestSubscriber.swift
@@ -7,6 +7,7 @@ class NotifySubscriptionsChangedRequestSubscriber {
     private let identityClient: IdentityClient
     private let kms: KeyManagementServiceProtocol
     private let logger: ConsoleLogging
+    private let groupKeychainStorage: KeychainStorageProtocol
     private let notifyStorage: NotifyStorage
     private let notifySubscriptionsBuilder: NotifySubscriptionsBuilder
     
@@ -16,6 +17,7 @@ class NotifySubscriptionsChangedRequestSubscriber {
         kms: KeyManagementServiceProtocol,
         identityClient: IdentityClient,
         logger: ConsoleLogging,
+        groupKeychainStorage: KeychainStorageProtocol,
         notifyStorage: NotifyStorage,
         notifySubscriptionsBuilder: NotifySubscriptionsBuilder
     ) {
@@ -24,6 +26,7 @@ class NotifySubscriptionsChangedRequestSubscriber {
         self.kms = kms
         self.logger = logger
         self.identityClient = identityClient
+        self.groupKeychainStorage = groupKeychainStorage
         self.notifyStorage = notifyStorage
         self.notifySubscriptionsBuilder = notifySubscriptionsBuilder
         subscribeForNofifyChangedRequests()
@@ -45,8 +48,19 @@ class NotifySubscriptionsChangedRequestSubscriber {
                 let subscriptions = try await notifySubscriptionsBuilder.buildSubscriptions(jwtPayload.subscriptions)
 
                 notifyStorage.replaceAllSubscriptions(subscriptions, account: account)
+
+                for subscription in subscriptions {
+                    try groupKeychainStorage.add(subscription.symKey, forKey: subscription.topic)
+                }
+
+                let topics = subscriptions.reduce([]) { $0 + [$1.topic] }
+
+                try await networkingInteractor.batchSubscribe(topics: topics)
+
+
+
+
 //                add keys to group keychain
-//                resubscribe for all the topics batch
 
                 var logProperties = ["rpcId": payload.id.string]
                 for (index, subscription) in subscriptions.enumerated() {
