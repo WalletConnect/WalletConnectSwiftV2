be6cd029afa5d96937668eea89c930e26b9ef303
Bartosz Rozwarski <bartus000@gmail.com>
2023-12-05 14:16:05 +0100

    refactor pairing tests

--- Example/IntegrationTests/Pairing/PairingTests.swift
+++ Example/IntegrationTests/Pairing/PairingTests.swift
@@ -23,13 +23,11 @@ final class PairingTests: XCTestCase {
 
     private var publishers = [AnyCancellable]()
 
-    func makeClientDependencies(prefix: String) -> (PairingClient, NetworkingInteractor, KeychainStorageProtocol, KeyValueStorage) {
+    func makeClients(prefix: String) -> (PairingClient, AuthClient) {
         let keychain = KeychainStorageMock()
         let keyValueStorage = RuntimeKeyValueStorage()
 
-        let relayLogger = ConsoleLogger(prefix: prefix + " [Relay]", loggingLevel: .debug)
-        let pairingLogger = ConsoleLogger(prefix: prefix + " [Pairing]", loggingLevel: .debug)
-        let networkingLogger = ConsoleLogger(prefix: prefix + " [Networking]", loggingLevel: .debug)
+        let logger = ConsoleLogger(prefix: name, loggingLevel: .debug)
 
         let relayClient = RelayClientFactory.create(
             relayHost: InputConfig.relayHost,
@@ -37,81 +35,45 @@ final class PairingTests: XCTestCase {
             keyValueStorage: RuntimeKeyValueStorage(),
             keychainStorage: keychain,
             socketFactory: DefaultSocketFactory(),
-            logger: relayLogger)
+            logger: logger)
 
         let networkingClient = NetworkingClientFactory.create(
             relayClient: relayClient,
-            logger: networkingLogger,
+            logger: logger,
             keychainStorage: keychain,
             keyValueStorage: keyValueStorage)
 
         let pairingClient = PairingClientFactory.create(
-            logger: pairingLogger,
+            logger: logger,
             keyValueStorage: keyValueStorage,
             keychainStorage: keychain,
             networkingClient: networkingClient)
+
+
         let clientId = try! networkingClient.getClientId()
-        networkingLogger.debug("My client id is: \(clientId)")
-        
-        return (pairingClient, networkingClient, keychain, keyValueStorage)
-    }
+        logger.debug("My client id is: \(clientId)")
 
-    func makeDappClients() {
-        let prefix = "🤖 Dapp: "
-        let (pairingClient, networkingInteractor, keychain, keyValueStorage) = makeClientDependencies(prefix: prefix)
-        let notifyLogger = ConsoleLogger(prefix: prefix + " [Notify]", loggingLevel: .debug)
-        appPairingClient = pairingClient
-        
-        appAuthClient = AuthClientFactory.create(
-            metadata: AppMetadata(name: name, description: "", url: "", icons: [""], redirect: AppMetadata.Redirect(native: "wcdapp://", universal: nil)),
-            projectId: InputConfig.projectId,
-            crypto: DefaultCryptoProvider(),
-            logger: notifyLogger,
-            keyValueStorage: keyValueStorage,
-            keychainStorage: keychain,
-            networkingClient: networkingInteractor,
-            pairingRegisterer: pairingClient,
-            iatProvider: IATProviderMock())
-    }
-
-    func makeWalletClients() {
-        let prefix = "🐶 Wallet: "
-        let (pairingClient, networkingInteractor, keychain, keyValueStorage) = makeClientDependencies(prefix: prefix)
-        let notifyLogger = ConsoleLogger(prefix: prefix + " [Notify]", loggingLevel: .debug)
-        let defaults = RuntimeKeyValueStorage()
-        walletPairingClient = pairingClient
-        let historyClient = HistoryClientFactory.create(
-            historyUrl: "https://history.walletconnect.com",
-            relayUrl: "wss://relay.walletconnect.com",
-            keyValueStorage: defaults,
-            keychain: keychain,
-            logger: notifyLogger
-        )
-        appAuthClient = AuthClientFactory.create(
+        let authClient = AuthClientFactory.create(
             metadata: AppMetadata(name: name, description: "", url: "", icons: [""], redirect: AppMetadata.Redirect(native: "", universal: nil)),
             projectId: InputConfig.projectId,
             crypto: DefaultCryptoProvider(),
-            logger: notifyLogger,
+            logger: logger,
             keyValueStorage: keyValueStorage,
             keychainStorage: keychain,
-            networkingClient: networkingInteractor,
+            networkingClient: networkingClient,
             pairingRegisterer: pairingClient,
             iatProvider: IATProviderMock())
-    }
 
-    func makeWalletPairingClient() {
-        let prefix = "🐶 Wallet: "
-        let (pairingClient, _, _, _) = makeClientDependencies(prefix: prefix)
-        walletPairingClient = pairingClient
+        return (pairingClient, authClient)
     }
 
     override func setUp() {
-        makeDappClients()
+        (appPairingClient, appAuthClient) = makeClients(prefix: "🤖 Dapp: ")
+        (walletPairingClient, walletAuthClient) = makeClients(prefix: "🐶 Wallet: ")
     }
 
     func testPing() async {
         let expectation = expectation(description: "expects ping response")
-        makeWalletClients()
         let uri = try! await appPairingClient.create()
         try? await walletPairingClient.pair(uri: uri)
         try! await walletPairingClient.ping(topic: uri.topic)
@@ -124,7 +86,6 @@ final class PairingTests: XCTestCase {
     }
 
     func testResponseErrorForMethodUnregistered() async {
-        makeWalletPairingClient()
         let expectation = expectation(description: "wallet responds unsupported method for unregistered method")
 
         appAuthClient.authResponsePublisher.sink { (_, response) in
@@ -140,8 +101,18 @@ final class PairingTests: XCTestCase {
 
         wait(for: [expectation], timeout: InputConfig.defaultTimeout)
     }
-
-    func testDisconnect() {
-        // TODO
-    }
+//
+//    func testDisconnect() {
+//
+//        let expectation = expectation(description: "wallet responds unsupported method for unregistered method")
+//
+//        appAuthClient.authResponsePublisher.sink { (_, response) in
+//            XCTAssertEqual(response, .failure(AuthError(code: 10001)!))
+//            expectation.fulfill()
+//        }.store(in: &publishers)
+//
+//        let uri = try! await appPairingClient.create()
+//
+//        try? await walletPairingClient.pair(uri: uri)
+//    }
 }
