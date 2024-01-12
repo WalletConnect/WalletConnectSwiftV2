859a85ad591a71f0e5edac8793d1c3cc14614c24
Artur Guseinov <arthur.guseinov@gmail.com>
2024-01-12 16:23:34 +0300

    NotifyGetNotificationsRequestPayload

--- Sources/WalletConnectHistory/Payloads/NotifyGetNotificationsRequestPayload.swift
+++ Sources/WalletConnectHistory/Payloads/NotifyGetNotificationsRequestPayload.swift
@@ -3,13 +3,15 @@ import Foundation
 struct NotifyGetNotificationsRequestPayload: JWTClaimsCodable {
 
     struct Claims: JWTClaims {
-        var iss: String
-        
-        var iat: UInt64
-        
-        var exp: UInt64
-        
-        var act: String?
+        let iat: UInt64
+        let exp: UInt64
+        let act: String?  // - `notify_get_notifications`
+        let iss: String   // - did:key of client identity key
+        let ksu: String   // - key server for identity key verification
+        let aud: String   // - did:key of dapp authentication key
+        let app: String   // - did:web of app domain that this request is associated with - Example: `did:web:app.example.com`
+        let lmt: UInt64   // - the max number of notifications to return. Maximum value is 50.
+        let aft: String?  // - the notification ID to start returning messages after. Null to start with the most recent notification
 
         static var action: String? {
             return "notify_get_notifications"
@@ -28,11 +30,28 @@ struct NotifyGetNotificationsRequestPayload: JWTClaimsCodable {
         }
     }
 
+    let identityKey: DIDKey
+    let keyserver: String
+    let dappAuthKey: DIDKey
+    let app: DIDWeb
+    let limit: UInt64
+    let after: String?
+
     init(claims: Claims) throws {
         fatalError()
     }
 
     func encode(iss: String) throws -> Claims {
-        fatalError()
+        return Claims(
+            iat: defaultIat(),
+            exp: expiry(days: 1),
+            act: Claims.action,
+            iss: identityKey.did(variant: .ED25519),
+            ksu: keyserver,
+            aud: dappAuthKey.did(variant: .ED25519),
+            app: app.did,
+            lmt: limit,
+            aft: after
+        )
     }
 }
