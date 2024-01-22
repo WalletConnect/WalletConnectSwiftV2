f0d60efae6d4e39dc4b62bd46111b01def8c215f
Artur Guseinov <arthur.guseinov@gmail.com>
2024-01-22 19:52:33 +0300

    Nullable icon and url parsing

--- Sources/WalletConnectNotify/Types/DataStructures/NotifyMessage.swift
+++ Sources/WalletConnectNotify/Types/DataStructures/NotifyMessage.swift
@@ -13,13 +13,24 @@ public struct NotifyMessage: Codable, Equatable {
         return Date(milliseconds: sent_at)
     }
 
-    public init(id: String, title: String, body: String, icon: String, url: String, type: String, sentAt: Date) {
+    public init(id: String, title: String, body: String, icon: String?, url: String?, type: String, sentAt: Date) {
         self.id = id
         self.title = title
         self.body = body
-        self.icon = icon
-        self.url = url
+        self.icon = icon ?? ""
+        self.url = url ?? ""
         self.type = type
         self.sent_at = UInt64(sentAt.millisecondsSince1970)
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.body = try container.decode(String.self, forKey: .body)
+        self.icon = try container.decodeIfPresent(String.self, forKey: .icon) ?? ""
+        self.url = try container.decodeIfPresent(String.self, forKey: .url) ?? ""
+        self.type = try container.decode(String.self, forKey: .type)
+        self.sent_at = try container.decode(UInt64.self, forKey: .sent_at)
     }
 }
