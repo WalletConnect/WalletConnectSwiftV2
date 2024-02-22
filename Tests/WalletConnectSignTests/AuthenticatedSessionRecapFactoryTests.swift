11c64879c2257efc35648ecdfe96fe91692ecf92
Bartosz Rozwarski <bartus000@gmail.com>
2024-02-22 15:42:53 +0100

    fix sign unit test

--- Tests/WalletConnectSignTests/AuthenticatedSessionRecapFactoryTests.swift
+++ Tests/WalletConnectSignTests/AuthenticatedSessionRecapFactoryTests.swift
@@ -4,15 +4,11 @@ import XCTest
 final class AuthenticatedSessionRecapFactoryTests: XCTestCase {
 
     func testAuthenticatedSessionRecapFactory() {
-        // {"att":{"eip155":{"request/eth_sendTransaction":[],"request/personal_sign":[]}}}
-        let recapUrn1 = "urn:recap:eyJhdHQiOnsiZWlwMTU1Ijp7InJlcXVlc3QvZXRoX3NlbmRUcmFuc2FjdGlvbiI6W10sInJlcXVlc3QvcGVyc29uYWxfc2lnbiI6W119fX0="
+        // {"att":{"eip155":{"request/eth_sendTransaction":[{}],"request/personal_sign":[{}]}}}
+        let referenceRecap = "urn:recap:eyJhdHQiOnsiZWlwMTU1Ijp7InJlcXVlc3QvZXRoX3NlbmRUcmFuc2FjdGlvbiI6W3t9XSwicmVxdWVzdC9wZXJzb25hbF9zaWduIjpbe31dfX19"
 
-        // {"att":{"eip155":{"request/personal_sign":[],"request/eth_sendTransaction":[]}}}
-        let recapUrn2 = "urn:recap:eyJhdHQiOnsiZWlwMTU1Ijp7InJlcXVlc3QvcGVyc29uYWxfc2lnbiI6W10sInJlcXVlc3QvZXRoX3NlbmRUcmFuc2FjdGlvbiI6W119fX0="
-
-        let urns = [recapUrn1, recapUrn2]
         let recapUrn = try! AuthenticatedSessionRecapUrnFactory.createNamespaceRecap(methods: ["personal_sign", "eth_sendTransaction"])
 
-        XCTAssertTrue(urns.contains(recapUrn))
+        XCTAssertEqual(recapUrn, referenceRecap)
     }
 }
