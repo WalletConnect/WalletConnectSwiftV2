19095370e494038835a0a9a3c81eafa187a3102e
Bartosz Rozwarski <bartus000@gmail.com>
2024-02-23 12:49:58 +0100

    add siwe only

--- Example/DApp/Modules/Sign/SignPresenter.swift
+++ Example/DApp/Modules/Sign/SignPresenter.swift
@@ -90,7 +90,21 @@ final class SignPresenter: ObservableObject {
             } catch {
                 ActivityIndicatorManager.shared.stop()
             }
+        }
+    }
 
+    @MainActor
+    func connectWalletWithSessionAuthenticateSIWEOnly() {
+        Task {
+            do {
+                ActivityIndicatorManager.shared.start()
+                let uri = try await Sign.instance.authenticate(.stub(methods: nil))
+                walletConnectUri = uri
+                ActivityIndicatorManager.shared.stop()
+                router.presentNewPairing(walletConnectUri: walletConnectUri!)
+            } catch {
+                ActivityIndicatorManager.shared.stop()
+            }
         }
     }
 
@@ -213,7 +227,8 @@ extension AuthRequestParams {
         exp: String? = nil,
         statement: String? = "I accept the ServiceOrg Terms of Service: https://app.web3inbox.com/tos",
         requestId: String? = nil,
-        resources: [String]? = ["urn:recap:eyJhdHQiOnsiaHR0cHM6Ly9ub3RpZnkud2FsbGV0Y29ubmVjdC5jb20vYWxsLWFwcHMiOnsiY3J1ZC9ub3RpZmljYXRpb25zIjpbe31dLCJjcnVkL3N1YnNjcmlwdGlvbnMiOlt7fV19fX0K", "ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/", "https://example.com/my-web2-claim.json"]
+        resources: [String]? = ["urn:recap:eyJhdHQiOnsiaHR0cHM6Ly9ub3RpZnkud2FsbGV0Y29ubmVjdC5jb20vYWxsLWFwcHMiOnsiY3J1ZC9ub3RpZmljYXRpb25zIjpbe31dLCJjcnVkL3N1YnNjcmlwdGlvbnMiOlt7fV19fX0K", "ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/", "https://example.com/my-web2-claim.json"],
+        methods: [String]? = ["eth_sign", "personal_sign", "eth_sendTransaction"]
     ) -> AuthRequestParams {
         return AuthRequestParams(
             domain: domain,
@@ -225,7 +240,7 @@ extension AuthRequestParams {
             statement: statement,
             requestId: requestId,
             resources: resources,
-            methods: ["eth_sign", "personal_sign", "eth_sendTransaction"]
+            methods: methods
         )
     }
 }
--- Example/DApp/Modules/Sign/SignView.swift
+++ Example/DApp/Modules/Sign/SignView.swift
@@ -57,6 +57,18 @@ struct SignView: View {
                                 }
 
                                 Button {
+                                    presenter.connectWalletWithSessionAuthenticateSIWEOnly()
+                                } label: {
+                                    Text("Connect Session Authenticate - SIWE only")
+                                        .font(.system(size: 16, weight: .semibold))
+                                        .foregroundColor(.white)
+                                        .padding(.horizontal, 16)
+                                        .padding(.vertical, 10)
+                                        .background(Color(red: 95/255, green: 159/255, blue: 248/255))
+                                        .cornerRadius(16)
+                                }
+
+                                Button {
                                     presenter.connectWalletWithWCM()
                                 } label: {
                                     Text("Connect with WalletConnectModal")
