48228303136da0a57ccfa538be1064d20bf54e8a
Bartosz Rozwarski <bartus000@gmail.com>
2024-02-26 09:40:39 +0100

    savepoint

--- Sources/WalletConnectSign/Auth/AuthProtocolMethod.swift
+++ Sources/WalletConnectSign/Auth/AuthProtocolMethod.swift
@@ -7,19 +7,3 @@ struct SessionAuthenticatedProtocolMethod: ProtocolMethod {
 
     let responseConfig = RelayConfig(tag: 1117, prompt: false, ttl: 3600)
 }
-
-struct PairingPingProtocolMethod: ProtocolMethod {
-    let method: String = "wc_pairingPing"
-
-    let requestConfig = RelayConfig(tag: 1002, prompt: false, ttl: 30)
-
-    let responseConfig = RelayConfig(tag: 1003, prompt: false, ttl: 30)
-}
-
-struct PairingDeleteProtocolMethod: ProtocolMethod {
-    let method: String = "wc_pairingDelete"
-
-    let requestConfig = RelayConfig(tag: 1000, prompt: false, ttl: 86400)
-
-    let responseConfig = RelayConfig(tag: 1001, prompt: false, ttl: 86400)
-}
