fce42b662cd366c37cca120d99793b50f75e08c6
Bartosz Rozwarski <bartus000@gmail.com>
2024-04-22 17:14:21 +0400

    handle error response

--- Sources/WalletConnectSign/Auth/Link/LinkSessionRequestResponseSubscriber.swift
+++ Sources/WalletConnectSign/Auth/Link/LinkSessionRequestResponseSubscriber.swift
@@ -14,6 +14,17 @@ class LinkSessionRequestResponseSubscriber {
     }
 
     func setupRequestSubscription() {
+        envelopesDispatcher.responseErrorSubscription(on: SessionRequestProtocolMethod())
+            .sink { [unowned self] (payload: ResponseSubscriptionErrorPayload<SessionType.RequestParams>) in
+                onSessionResponse?(Response(
+                    id: payload.id,
+                    topic: payload.topic,
+                    chainId: payload.request.chainId.absoluteString,
+                    result: .error(payload.error)
+                ))
+            }
+            .store(in: &publishers)
+
         envelopesDispatcher.responseSubscription(on: SessionRequestProtocolMethod())
             .sink { [unowned self] (payload: ResponseSubscriptionPayload<SessionType.RequestParams, AnyCodable>) in
                 Task(priority: .high) {
