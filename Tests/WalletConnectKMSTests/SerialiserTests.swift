d820c4a849c4b56de7b7651092141fb039d23dfe
Bartosz Rozwarski <bartus000@gmail.com>
2024-05-27 14:03:36 +0200

    fix kms tests

--- Tests/WalletConnectKMSTests/SerialiserTests.swift
+++ Tests/WalletConnectKMSTests/SerialiserTests.swift
@@ -23,7 +23,7 @@ final class SerializerTests: XCTestCase {
         _ = try! myKms.createSymmetricKey(topic)
         let messageToSerialize = "todo - change for request object"
         let serializedMessage = try! mySerializer.serialize(topic: topic, encodable: messageToSerialize, envelopeType: .type0)
-        let (deserializedMessage, _, _): (String, String?, Data) = mySerializer.tryDeserialize(topic: topic, codingType: .base64Encoded(serializedMessage))!
+        let (deserializedMessage, _, _): (String, String?, Data) = mySerializer.tryDeserialize(topic: topic, codingType: .base64Encoded, envelopeString: serializedMessage)!
         XCTAssertEqual(messageToSerialize, deserializedMessage)
     }
 
@@ -39,7 +39,7 @@ final class SerializerTests: XCTestCase {
         let serializedMessage = try! peerSerializer.serialize(topic: topic, encodable: messageToSerialize, envelopeType: .type1(pubKey: peerPubKey.rawRepresentation))
         print(agreementKeys.sharedKey.hexRepresentation)
         // -----------Me Deserialising -------------------
-        let (deserializedMessage, _, _): (String, String?, Data) = mySerializer.tryDeserialize(topic: topic, codingType: .base64Encoded(serializedMessage))!
+        let (deserializedMessage, _, _): (String, String?, Data) = mySerializer.tryDeserialize(topic: topic, codingType: .base64Encoded, envelopeString: serializedMessage)!
         XCTAssertEqual(messageToSerialize, deserializedMessage)
     }
 
@@ -54,7 +54,7 @@ final class SerializerTests: XCTestCase {
         }
 
         // Deserialize the serialized message back into the original object
-        guard let (deserializedMessage, _, _): (String, String?, Data) = mySerializer.tryDeserialize(topic: "", codingType: .base64UrlEncoded(serializedMessage)) else {
+        guard let (deserializedMessage, _, _): (String, String?, Data) = mySerializer.tryDeserialize(topic: "", codingType: .base64UrlEncoded, envelopeString: serializedMessage) else {
             XCTFail("Deserialization failed for Type 2 envelope.")
             return
         }
@@ -75,7 +75,7 @@ final class SerializerTests: XCTestCase {
         _ = try! myKms.createSymmetricKey(topic)
         let serialized = try! mySerializer.serialize(topic: topic, encodable: request, envelopeType: .type0)
 
-        if let result = mySerializer.tryDeserializeRequestOrResponse(topic: topic, codingType: .base64Encoded(serialized)) {
+        if let result = mySerializer.tryDeserializeRequestOrResponse(topic: topic, codingType: .base64Encoded, envelopeString: serialized) {
             switch result {
             case .left(let result):
                 XCTAssertEqual(result.request.method, request.method)
@@ -96,7 +96,7 @@ final class SerializerTests: XCTestCase {
         _ = try! myKms.createSymmetricKey(topic)
         let serialized = try! mySerializer.serialize(topic: topic, encodable: response, envelopeType: .type0)
 
-        if let result = mySerializer.tryDeserializeRequestOrResponse(topic: topic, codingType: .base64Encoded(serialized)) {
+        if let result = mySerializer.tryDeserializeRequestOrResponse(topic: topic, codingType: .base64Encoded, envelopeString: serialized) {
             switch result {
             case .right(let result):
                 XCTAssertEqual(result.response, response)
@@ -115,7 +115,7 @@ final class SerializerTests: XCTestCase {
         // Assuming serialize can accept invalidData for the purpose of this test
         let serialized = try! mySerializer.serialize(topic: topic, encodable: invalidData, envelopeType: .type0)
 
-        let result = mySerializer.tryDeserializeRequestOrResponse(topic: topic, codingType: .base64Encoded(serialized))
+        let result = mySerializer.tryDeserializeRequestOrResponse(topic: topic, codingType: .base64Encoded, envelopeString: serialized)
         XCTAssertNil(result, "Deserialization should fail for invalid data")
     }
 
