7eae272a9a0eef54bfe0897fe5c267dbb302568d
Bartosz Rozwarski <bartus000@gmail.com>
2024-06-03 10:15:29 +0200

    update w3m

--- Sources/WalletConnectSign/Auth/Services/App/AuthResponseSubscriber.swift
+++ Sources/WalletConnectSign/Auth/Services/App/AuthResponseSubscriber.swift
@@ -140,7 +140,6 @@ class AuthResponseSubscriber {
 
     private func getTransportTypeUpgradeIfPossible(peerMetadata: AppMetadata, requestId: RPCID) -> WCSession.TransportType {
 //        upgrade to link mode only if dapp requested universallink because dapp may not be prepared for handling a response - add this to doc]
-        // remove record
 
         if let peerRedirect = peerMetadata.redirect,
            let peerLinkMode = peerRedirect.linkMode,
