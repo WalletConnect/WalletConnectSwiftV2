a4e1f36c47ecc586a496a9754bc97a01305e40b8
Bartosz Rozwarski <bartus000@gmail.com>
2024-07-09 13:56:54 +0200

    fix relay tests

--- Tests/RelayerTests/Mocks/DispatcherMock.swift
+++ Tests/RelayerTests/Mocks/DispatcherMock.swift
@@ -4,6 +4,8 @@ import Combine
 @testable import WalletConnectRelay
 
 class DispatcherMock: Dispatching {
+    var isSocketConnected: Bool = true
+    
 
     private var publishers = Set<AnyCancellable>()
     private let socketConnectionStatusPublisherSubject = CurrentValueSubject<SocketConnectionStatus, Never>(.disconnected)
