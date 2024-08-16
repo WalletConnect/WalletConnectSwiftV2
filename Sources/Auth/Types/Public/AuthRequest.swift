fca72e1210c554ae95b3449fe4328b07649f8cbe
Bartosz Rozwarski <bartus000@gmail.com>
2024-08-16 12:33:14 +0200

    remove auth

--- Sources/Auth/Types/Public/AuthRequest.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public struct AuthRequest: Equatable, Codable {
-    public let id: RPCID
-    public let topic: String
-    public let payload: AuthPayloadStruct
-    public let requester: AppMetadata
-}
