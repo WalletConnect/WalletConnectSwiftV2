661b295c62b2acd28060e6ad60d6b5847d943254
Alfreedom <00tango.bromine@icloud.com>
2024-08-28 11:13:19 +0200

    Removed production wallet

--- Example/DApp/SceneDelegate.swift
+++ Example/DApp/SceneDelegate.swift
@@ -128,15 +128,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     linkMode: "https://lab.web3modal.com/rn_walletkit"
                 ),
                 .init(
-                    id: "flutter-sample",
-                    name: "FL Sample Wallet",
-                    homepage: "https://walletconnect.com/",
-                    imageUrl: "https://avatars.githubusercontent.com/u/37784886?s=200&v=4",
-                    order: 1,
-                    mobileLink: "wcflutterwallet://",
-                    linkMode: "https://lab.web3modal.com/walletkit_flutter"
-                ),
-                .init(
                     id: "flutter-sample-internal",
                     name: "FL Sample Wallet (internal)",
                     homepage: "https://walletconnect.com/",
